8e927d6001b3ca14e434c908fbbf776cd5c181cb
Kruy Vanna <kruyvanna@gmail.com>
2013-07-11 16:40:44 +0900

    delete unused files

--- app/js/directives.js
+++ app/js/directives.js
@@ -11,73 +11,22 @@ directive('appVersion', ['version',
 		};
 	}
 ])
-	.directive('zippy', function() {
-	return {
-		restrict: 'C',
-		// This HTML will replace the zippy directive.
-		replace: true,
-		transclude: true,
-		scope: {
-			title: '@zippyTitle'
-		},
-		template: '<div>' + '<div class="title">{{title}}</div>' + '<div class="body" ng-transclude></div>' + '</div>',
-		// The linking function will add behavior to the template
-		link: function(scope, element, attrs) {
-			// Title element
-			var title = angular.element(element.children()[0]),
-				// Opened / closed state
-				opened = true;
-
-			// Clicking on title should open/close the zippy
-			title.bind('click', toggle);
-
-			// Toggle the closed/opened state
-
-			function toggle() {
-				opened = !opened;
-				element.removeClass(opened ? 'closed' : 'opened');
-				element.addClass(opened ? 'opened' : 'closed');
-			}
-
-			// initialize the zippy
-			toggle();
-		}
-	}
-}).directive('monacaNavigation', function() {
+	.directive('monacaNavigation', function() {
 	return {
 		restrict: 'E',
-		// This HTML will replace the zippy directive.
 		replace: false,
 		transclude: true,
-		
+
 		templateUrl: 'templates/navigation.html',
 		// The linking function will add behavior to the template
 		link: function(scope, element, attrs) {
 			// Title element
 			var childSources = [];
-			var title = angular.element(element.children()[0]),
-				// Opened / closed state
-				opened = true;
-
-				scope.$watch(attrs.childSource, function(value) {			        
-			        childSources.push(value);
-			        console.log('sources,', childSources);
-			    });
-
-
-			// Clicking on title should open/close the zippy
-			// title.bind('click', toggle);
-
-			// // Toggle the closed/opened state
-
-			// function toggle() {
-			// 	opened = !opened;
-			// 	element.removeClass(opened ? 'closed' : 'opened');
-			// 	element.addClass(opened ? 'opened' : 'closed');
-			// }
-
-			// // initialize the zippy
-			// toggle();
+			var title = angular.element(element.children()[0]);
+			scope.$watch(attrs.childSource, function(value) {
+				childSources.push(value);
+				console.log('sources,', childSources);
+			});
 		}
 	}
 });
--- app/js/navigation/directive.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Directives */
-
-
-// angular.module('myApp.directives', [])
-	
-// });
