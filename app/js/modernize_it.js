1bf3f242514d54ffff27849cf1cb2e99acf9d986
Kruy Vanna <kruyvanna@gmail.com>
2013-07-30 15:52:19 +0900

    add modernizer

---
+++ app/js/modernize_it.js
@@ -0,0 +1,20 @@
+Modernizr.testStyles('#modernizr { -webkit-overflow-scrolling:touch }', function(elem, rule) {
+	Modernizr.addTest(
+		'overflowtouch',
+		window.getComputedStyle && window.getComputedStyle(elem).getPropertyValue('-webkit-overflow-scrolling') == 'touch');
+});
+
+yepnope({
+	test: Modernizr.svg,
+	nope: ['css/topcoat-mobile-light-polyfill.css']
+});
+
+yepnope({
+	test: Modernizr.overflowtouch,
+	nope: ['css/sliding_menu_polyfill.css']
+});
+
+yepnope({
+	test: Modernizr.overflowtouch,
+	nope: ['js/vendor/iscroll-lite.js']
+});
