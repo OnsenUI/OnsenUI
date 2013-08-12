64be8787f8f5dd96c7fc1dd6f2a67a72f9f318fc
Kruy Vanna <kruyvanna@gmail.com>
2013-08-12 16:14:08 +0900

    fix monaca-scroller infinite-scroll to support android

--- app/framework/directives/scroller.js
+++ app/framework/directives/scroller.js
@@ -3,7 +3,7 @@
 (function() {
 	var directives = angular.module('monaca.directives'); // no [] -> referencing existing module
 
-	directives.directive('monacaScroller', function(MONACA_CONSTANTS) {
+	directives.directive('monacaScroller', function(MONACA_CONSTANTS, $timeout) {
 		return {
 			restrict: 'E',
 			replace: false,
@@ -20,30 +20,54 @@
 				var e = scrollerWrapper[0];
 
 				scrollerWrapper.bind('scroll', function() {
-					if(scope.infinitScrollEnable){
+					if (scope.infinitScrollEnable) {
 						var scrollTopAndOffsetHeight = e.scrollTop + e.offsetHeight;
 						var scrollHeightMinusOffset = e.scrollHeight - offset;
-						console.log('scrollTopAndOffsetHeight: ' + scrollTopAndOffsetHeight + ', scrollHeightMinusOffset: ' + scrollHeightMinusOffset);
+						// console.log('scrollTopAndOffsetHeight: ' + scrollTopAndOffsetHeight + ', scrollHeightMinusOffset: ' + scrollHeightMinusOffset);
 						if (scrollTopAndOffsetHeight >= scrollHeightMinusOffset) {
 							console.log('we are there!');
 							scope.onScrolled();
 						} else {
-							console.log('not yet there!');
+							// console.log('not yet there!');
 						}
-					}					
+					}
 				});
 
-
 				// IScroll for Android
-				setTimeout(function() {
-					var wrapper = element[0].querySelector('.scroller-wrapper');
-					var iScroll = new IScroll(wrapper, {
-						momentum: true,
-						bounce: true,
-						hScrollbar: false,
-						vScrollbar: false
-					});
-				}, 1000);
+				if (!Modernizr.overflowtouch) {
+					$timeout(function() {
+						var wrapper = element[0].querySelector('.scroller-wrapper');
+						var iScroll = new IScroll(wrapper, {
+							momentum: true,
+							bounce: true,
+							hScrollbar: false,
+							vScrollbar: false
+						});
+
+						iScroll.on('scrollStart', function(e) {
+							// console.log('scroll start');
+							// console.log('scrollerHeight:' + iScroll.maxScrollY + ', distantY: ' + iScroll.y);
+
+							var scrolled = iScroll.y - offset;
+							// console.log('scrollerHeight:' + iScroll.maxScrollY + ', distantY: ' + iScroll.y + ', scrolled:' + scrolled);
+							if (scrolled < iScroll.maxScrollY) {								
+								// TODO: find a better way to know when content is upated so we can refresh
+								iScroll.refresh();
+							}
+							
+						});
+
+						iScroll.on('scrollEnd', function(e) {
+							// console.log('scroll end');
+							var scrolled = iScroll.y - offset;
+							// console.log('scrollerHeight:' + iScroll.maxScrollY + ', distantY: ' + iScroll.y + ', scrolled:' + scrolled);
+							if (scrolled < iScroll.maxScrollY) {
+								console.log('we are there!');
+								scope.onScrolled();								
+							}
+						});
+					}, 0);
+				}
 
 			}
 		};
