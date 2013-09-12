8ff8a2f1171c3b1666916c7a323ebfcc91d26199
Kruy Vanna <kruyvanna@gmail.com>
2013-09-12 14:21:36 +0900

    add docs url

--- app/framework/directives/module.js
+++ app/framework/directives/module.js
@@ -3,8 +3,8 @@
 
 	directiveModules.factory('ONSEN_CONSTANTS', function() {
 		var CONSTANTS = {
-			DIRECTIVE_TEMPLATE_URL: "plugins/onsenui/templates" // production
-			// DIRECTIVE_TEMPLATE_URL: "lib/maccha/app/templates" // test
+			// DIRECTIVE_TEMPLATE_URL: "plugins/onsenui/templates" // production
+			DIRECTIVE_TEMPLATE_URL: "/onsen/demo/app/lib/maccha/app/templates" // test
 		};
 
 		return CONSTANTS;
