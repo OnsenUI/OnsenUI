1d33e2214dedf412d409d48ec467b30a3986da5c
Kruy Vanna <kruyvanna@gmail.com>
2014-01-07 23:03:45 +0900

    update built files

--- project_templates/tab_bar/lib/onsen/onsenui.js
+++ project_templates/tab_bar/lib/onsen/onsenui.js
@@ -16973,7 +16973,8 @@ angular.module("templates/radio_button.tpl", []).run(["$templateCache", function
 
 angular.module("templates/screen.tpl", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("templates/screen.tpl",
-    "<ng-include class=\"screen max absolute\" src=\"screenItem.source\" ng-animate=\"animation\"></ng-include>");
+    "<div class=\"screen\">\n" +
+    "</div>");
 }]);
 
 angular.module("templates/scroller.tpl", []).run(["$templateCache", function($templateCache) {
@@ -17559,7 +17560,7 @@ limitations under the License.
 			scope: {
 				page: '@'
 			},
-			templateUrl: ONSEN_CONSTANTS.DIRECTIVE_TEMPLATE_URL + '/screen.tpl',
+			
 			// The linking function will add behavior to the template
 			link: function(scope, element, attrs) {
 				var screenItems = [];
@@ -17575,10 +17576,20 @@ limitations under the License.
 						}
 					},
 
-					animateIn: function(pager) {
-						pager.attr("class", "screen unmodal");
-						element[0].offsetWidth;
-						pager.attr("class", "screen transition center");
+					animateInBehindPage: function(){
+						var behindPage = screenItems[screenItems.length - 1];
+						behindPage.attr('class', 'screen-page transition modal-behind');
+					},
+
+					animateInCurrentPage: function(pager) {
+						pager.attr("class", "screen-page unmodal");
+						element[0].offsetWidth;						
+						pager.attr("class", "screen-page transition center");
+					},
+
+					animateOutBehindPage: function(){
+						var behindPage = screenItems[screenItems.length - 1];
+						behindPage.attr('class', 'screen-page transition');
 					},
 
 					isEmpty: function() {
@@ -17592,14 +17603,15 @@ limitations under the License.
 								method: "GET"
 							}).success(function(data, status, headers, config) {
 								var page = angular.element('<div></div>');
-								page.addClass('page');
+								page.addClass('screen-page');
 								var templateHTML = angular.element(data);
 								page.append(templateHTML);
 								var pager = $compile(page)(scope);
 								element.append(pager);
 
-								if (!this.isEmpty()) {
-									this.animateIn(pager);
+								if (!this.isEmpty()) {									
+									this.animateInBehindPage();
+									this.animateInCurrentPage(pager);
 								}
 
 								screenItems.push(pager);
@@ -17610,11 +17622,12 @@ limitations under the License.
 
 						scope.ons.screen.dismissPage = function() {
 							var currentPage = screenItems.pop();
-							currentPage.attr("class", "screen transition unmodal");
+							this.animateOutBehindPage();
+							currentPage.attr("class", "screen-page transition unmodal");
 							currentPage[0].addEventListener('webkitTransitionEnd', function transitionEnded(e) {
 								currentPage.remove();
-								currentPage[0].removeEventListner(transitionEnded);
-							});
+								currentPage[0].removeEventListener(transitionEnded);
+							});							
 						}.bind(this);
 					}
 				});
