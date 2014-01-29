e849e4f9c1be4f2447bcb2365b39109105189e97
Kruy Vanna <kruyvanna@gmail.com>
2014-01-29 16:14:29 +0900

    if-platform, add browser values

--- framework/directives/if_platform.js
+++ framework/directives/if_platform.js
@@ -12,11 +12,39 @@
 			templateUrl: ONSEN_CONSTANTS.DIRECTIVE_TEMPLATE_URL + '/if_platform.tpl',
 			link: function($scope, element, attrs) {
 
-				var platform;
+				var platform;				
+
+				var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+				    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+				var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+				var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+				    // At least Safari 3+: "[object HTMLElementConstructor]"
+				var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+				var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+				if(isOpera){
+					platform = "opera";
+				}
+
+				if(isFirefox){
+					platform = "firefox";
+				}
+
+				if(isSafari){
+					platform = "safari";
+				}
+
+				if(isChrome){
+					platform = "chrome";
+				}
+
+				if(isIE){
+					platform = "ie";
+				}
+
 				if (navigator.userAgent.match(/Android/i)) {
 					platform = "android";
 				}
-
 
 				if (navigator.userAgent.match(/BlackBerry/i)) {
 					platform = "blackberry";
