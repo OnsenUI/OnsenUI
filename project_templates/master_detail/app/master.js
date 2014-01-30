70497936eccabec05c8434b61989bc019ebd01a5
Kruy Vanna <kruyvanna@gmail.com>
2014-01-30 19:39:13 +0900

    upgrade master_detail project template

--- project_templates/master_detail/app/master.js
+++ project_templates/master_detail/app/master.js
@@ -4,6 +4,6 @@ function MasterController($scope, Data){
     $scope.showDetail = function(index){
         var selectedItem = Data.items[index];
         Data.selectedItem = selectedItem;
-        $scope.ons.navigator.pushPage('detail.html', selectedItem.title);
+        $scope.ons.navigator.pushPage('detail.html', { title : selectedItem.title });
     }
 }
