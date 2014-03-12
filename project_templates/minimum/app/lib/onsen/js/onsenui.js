7832957996090b297bb76cd129c428cf6b17b789
Kruy Vanna <kruyvanna@gmail.com>
2014-03-12 21:02:48 +0900

    bump up version to 1.0.3

--- project_templates/minimum/app/lib/onsen/js/onsenui.js
+++ project_templates/minimum/app/lib/onsen/js/onsenui.js
@@ -1,4 +1,4 @@
-/*! onsenui - v1.0.2 - 2014-03-12 */
+/*! onsenui - v1.0.3 - 2014-03-12 */
 angular.module('templates-main', ['templates/bottom_toolbar.tpl', 'templates/button.tpl', 'templates/checkbox.tpl', 'templates/column.tpl', 'templates/icon.tpl', 'templates/if_orientation.tpl', 'templates/if_platform.tpl', 'templates/list.tpl', 'templates/list_item.tpl', 'templates/navigator.tpl', 'templates/navigator_toolbar.tpl', 'templates/page.tpl', 'templates/radio_button.tpl', 'templates/row.tpl', 'templates/screen.tpl', 'templates/scroller.tpl', 'templates/search_input.tpl', 'templates/select.tpl', 'templates/sliding_menu.tpl', 'templates/split_view.tpl', 'templates/tab_bar.tpl', 'templates/tab_bar_item.tpl', 'templates/text_area.tpl', 'templates/text_input.tpl']);
 
 angular.module("templates/bottom_toolbar.tpl", []).run(["$templateCache", function($templateCache) {
@@ -1379,7 +1379,13 @@ limitations under the License.
 							var pageScope = navigator.createPageScope();				
 											
 							transclude(pageScope, function(compiledPageContent) {
-								var options = {};
+								var options = {
+									title: scope.title,
+									leftButtonIcon: scope.initialLeftButtonIcon,
+									rightButtonIcon: scope.rightButtonIcon,
+									onLeftButtonClick: scope.onLeftButtonClick,
+									onRightButtonClick: scope.onRightButtonClick
+								};
 								var compiledPage = navigator.generatePageEl(angular.element(compiledPageContent), options);
 								navigator._pushPageDOM('', compiledPageContent[0], compiledPage, pageScope, options);
 							});
