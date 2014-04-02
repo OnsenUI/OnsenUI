790ecada402a565d332192607e1148489f33a74f
Kruy Vanna <kruyvanna@gmail.com>
2014-04-02 19:57:21 +0900

    indent 2 spaces

--- framework/js/modernize_it.js
+++ framework/js/modernize_it.js
@@ -1,9 +1,9 @@
 (function() {
-	'use strict';
-	Modernizr.testStyles('#modernizr { -webkit-overflow-scrolling:touch }', function(elem, rule) {
-		Modernizr.addTest(
-			'overflowtouch',
-			window.getComputedStyle && window.getComputedStyle(elem).getPropertyValue('-webkit-overflow-scrolling') == 'touch');
-	});
+  'use strict';
+  Modernizr.testStyles('#modernizr { -webkit-overflow-scrolling:touch }', function(elem, rule) {
+    Modernizr.addTest(
+      'overflowtouch',
+      window.getComputedStyle && window.getComputedStyle(elem).getPropertyValue('-webkit-overflow-scrolling') == 'touch');
+  });
 
 })();
