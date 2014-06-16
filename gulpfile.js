30b8776998cf030ce93d0fd9ee60c0b7a7c16704
Mitsunori Kubota <mitsunori@asial.co.jp>
2014-06-16 12:38:30 +0900

    Fixed forbidden error on browsersync.

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ var browserSync = require('browser-sync');
 gulp.task('browser-sync', function() {
   browserSync.init(null, {
     server: {
-      baseDir: './',
+      baseDir: __dirname + '/',
       directory: true
     },
     ghostMode: false
