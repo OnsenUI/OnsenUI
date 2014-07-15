5beee0c3fb349816bedbf26301af2c0d0629729a
Masahiro Tanaka <masahiro@asial.co.jp>
2014-07-16 00:05:39 +0900

    Added more description for ons-navigator

--- docs/tag-defs/index.js
+++ docs/tag-defs/index.js
@@ -3,4 +3,11 @@ module.exports = [
   { name: 'example' },
   { name: 'id' },
   { name: 'note' },
+  {
+    name: 'seealso',
+    multi: true,
+    docProperty: 'seealsos'
+  },
+  { name: 'guide' },
+  { name: 'codepen' }
 ];
