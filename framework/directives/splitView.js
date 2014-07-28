7d373c4009021de8679daa4977f5cb7fd4f3042f
Mitsunori Kubota <mitsunori@asial.co.jp>
2014-07-28 17:07:45 +0900

    Trivial fix for split-view doccomments.

--- framework/directives/splitView.js
+++ framework/directives/splitView.js
@@ -4,16 +4,18 @@
  * @name ons-split-view
  * @description
  * Divides the screen into left and right section. This component can also act as sliding menu which can be controlled by 'collapse' attribute
+ *
  * @param secondary-page The url of the page on the left
  * @param main-page The url of the page on the right
  * @param main-page-width Main page's width percentage. The width of secondary page take the remaining percentage
  * @param collapse [Deprecated] Specify the collapse behavior. Valid values are [portrait/landscape/width ##px]. "portrait" means the view will collapse when device is in portrait orien0ation. "landscape" means the view will collapse when device is in landscape orientation. "width ##px" means the view will collapse when the window width is smaller than the specified ##px
  * @param var Variable name to refer this split view.
+ *
  * @property setMainPage(pageUrl) Show the page specified in pageUrl in the right section
  * @property setSecondaryPage(pageUrl) Show the page specified in pageUrl in the left section
- * @property [Deprecated] open() Reveal the secondary page if the view is in collapse mode
- * @property [Deprecated] close() hide the secondary page if the view is in collapse mode
- * @property [Deprecated] toggle() Reveal the secondary page if it is currently hidden, otherwies, reveal it
+ * @property open() [Deprecated] Reveal the secondary page if the view is in collapse mode
+ * @property close() [Deprecated] hide the secondary page if the view is in collapse mode
+ * @property toggle() [Deprecated] Reveal the secondary page if it is currently hidden, otherwies, reveal it
  * @codepen nKqfv {wide}
  * @guide multi-screen-support Multi screen support
  */
