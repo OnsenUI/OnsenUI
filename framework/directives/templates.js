3a68076b43f529f14bd6dc22811309ec5c84e22e
Mitsunori Kubota <mitsunori@asial.co.jp>
2014-09-12 12:22:16 +0900

    Fixed broken layout on ons-back-button's icon.

--- framework/directives/templates.js
+++ framework/directives/templates.js
@@ -5,7 +5,7 @@ app.run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("templates/back_button.tpl",
     "<span class=\"toolbar-button--quiet {{modifierTemplater('toolbar-button--quiet--*')}}\" ng-click=\"$root.ons.findParentComponentUntil('ons-navigator', $event).popPage()\" style=\"height: 44px; line-height: 0; padding: 0; position: relative;\">\n" +
-    "  <i class=\"ion-ios7-arrow-back ons-back-button__icon\" style=\"vertical-align: middle; line-height: 44px; font-size: 36px; margin-left: 8px; margin-right: 2px; height: 44px; width: 16px !important; display: inline-block;\"></i><span style=\"vertical-align: top; display: inline-block; line-height: 44px; height: 44px;\" class=\"back-button__label\"></span>\n" +
+    "  <i class=\"ion-ios7-arrow-back ons-back-button__icon\" style=\"vertical-align: top; background-color: transparent; height: 44px; line-height: 44px; font-size: 36px; margin-left: 8px; margin-right: 2px; width: 16px; display: inline-block; padding-top: 1px;\"></i><span style=\"vertical-align: top; display: inline-block; line-height: 44px; height: 44px;\" class=\"back-button__label\"></span>\n" +
     "</span>\n" +
     "");
 }]);
