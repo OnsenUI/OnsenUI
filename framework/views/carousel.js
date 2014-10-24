c880aa9aaf69b32714211eb44b5005e1b2a4fa64
Mitsunori Kubota <mitsunori@asial.co.jp>
2014-10-24 23:15:33 +0900

    Implemented "ons-carousel" element roughly.

--- framework/views/carousel.js
+++ framework/views/carousel.js
@@ -20,13 +20,84 @@ limitations under the License.
 
   var module = angular.module('onsen');
 
-  module.factory('CarouselView', function($http, $parse, $templateCache, $compile, $onsen, $timeout) {
+  module.factory('CarouselView', function($onsen) {
+
+    var VerticalModeTrait = {
+
+      _getScrollDelta: function(event) {
+        return event.gesture.deltaY;
+      },
+
+      _getScrollVelocity: function(event) {
+        return event.gesture.velocityY;
+      },
+
+      _getElementSize: function() {
+        return this._element[0].getBoundingClientRect().height;
+      },
+
+      _generateScrollTransform: function(scroll) {
+        return 'translate3d(0px, ' + -scroll + 'px, 0px)';
+      },
+      
+      _layoutCarouselItems: function() {
+        var children = this._getCarouselItemElements();
+
+        var sizeAttr = this._getCarouselItemSizeAttr();
+        var sizeInfo = this._decomposeSizeString(sizeAttr);
+        
+        for (var i = 0; i < children.length; i++) {
+          angular.element(children[i]).css({
+            position: 'absolute',
+            height: sizeAttr,
+            width: '100%',
+            visibility: 'visible',
+            left: '0px',
+            top: (i * sizeInfo.number) + sizeInfo.unit
+          });
+        }
+      },
+    };
+
+    var HorizontalModeTrait = {
+
+      _getScrollDelta: function(event) {
+        return event.gesture.deltaX;
+      },
+
+      _getScrollVelocity: function(event) {
+        return event.gesture.velocityX;
+      },
+
+      _getElementSize: function() {
+        return this._element[0].getBoundingClientRect().width;
+      },
+
+      _generateScrollTransform: function(scroll) {
+        return 'translate3d(' + -scroll + 'px, 0px, 0px)';
+      },
+
+      _layoutCarouselItems: function() {
+        var children = this._getCarouselItemElements();
+
+        var sizeAttr = this._getCarouselItemSizeAttr();
+        var sizeInfo = this._decomposeSizeString(sizeAttr);
+        
+        for (var i = 0; i < children.length; i++) {
+          angular.element(children[i]).css({
+            position: 'absolute',
+            width: sizeAttr,
+            height: '100%',
+            top: '0px',
+            visibility: 'visible',
+            left: (i * sizeInfo.number) + sizeInfo.unit
+          });
+        }
+      },
+
+    };
 
     /**
-     * scroll
-     * |----------|----------|
-     * |          |          |
-     * |----------|----------|
      * @class CarouselView
      */
     var CarouselView = Class.extend({
@@ -47,6 +118,11 @@ limitations under the License.
       _doorLock: undefined,
 
       /**
+       * @member {Number}
+       */
+      _scroll: undefined,
+
+      /**
        * @param {Object} scope
        * @param {jqLite} element
        * @param {Object} attrs
@@ -59,121 +135,357 @@ limitations under the License.
         this._doorLock = new DoorLock();
         this._scroll = 0;
 
+        this._bindedOnDrag = this._onDrag.bind(this);
+        this._bindedOnDragEnd = this._onDragEnd.bind(this);
+
+        this._mixin(this._isVertical() ? VerticalModeTrait : HorizontalModeTrait);
+
         this._prepareEventListeners();
-        this._layoutItems();
+        this._layoutCarouselItems();
+        this._setupInitialIndex();
 
         this._scope.$on('$destroy', this._destroy.bind(this));
       },
 
-      _layoutItems: function() {
-        var children = this._element.children();
-        
-        for (var i = 0; i < children.length; i++) {
-          angular.element(children[i]).css({
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            top: '0px',
-            left: (i * 100) + '%'
-          });
+      /**
+       * @return {Number}
+       */
+      _getCarouselItemSize: function() {
+        var sizeAttr = this._getCarouselItemSizeAttr();
+        var sizeInfo = this._decomposeSizeString(sizeAttr);
+        var elementSize = this._getElementSize();
+
+        if (sizeInfo.unit === '%') {
+          return Math.round(sizeInfo.number / 100 * elementSize);
+        } else if (sizeInfo.unit === 'px') {
+          return sizeInfo.number;
+        } else {
+          throw new Error('Invalid state');
         }
+      },
+
+      /**
+       * @return {Number}
+       */
+      _getInitialIndex: function() {
+        var index = parseInt(this._element.attr('initial-index'), 10);
+
+        if (typeof index === 'number' && !isNaN(index)) {
+          return Math.max(Math.min(index, this._getCarouselItemCount() - 1), 0);
+        } else {
+          return 0;
+        }
+      },
+
+      /**
+       * @return {String}
+       */
+      _getCarouselItemSizeAttr: function() {
+        var attrName = 'item-' + (this._isVertical() ? 'height' : 'width');
+        var itemSizeAttr = ('' + this._element.attr(attrName)).trim();
+
+        return itemSizeAttr.match(/^\d+(px|%)$/) ? itemSizeAttr : '100%';
+      },
+
+      /**
+       * @return {Object}
+       */
+      _decomposeSizeString: function(size) {
+        var matches = size.match(/^(\d+)(px|%)/);
+
+        return {
+          number: parseInt(matches[1], 10),
+          unit: matches[2],
+        };
+      },
+
+      _setupInitialIndex: function() {
+        this._scroll = this._getCarouselItemSize() * this._getInitialIndex();
+        this._scrollTo(this._scroll);
+      },
+
+      /**
+       * @param {Boolean} swipable
+       */
+      setSwipable: function(swipable) {
+        if (swipable) {
+          this._element[0].setAttribute('swipable', '');
+        } else {
+          this._element[0].removeAttribute('swipable');
+        }
+      },
+
+      /**
+       * @return {Boolean}
+       */
+      isSwipable: function() {
+        return this._element[0].hasAttribute('swipable');
+      },
+
+      /**
+       * @param {Boolean} draggable
+       */
+      setDraggable: function(draggable) {
+        if (draggable) {
+          this._element[0].setAttribute('draggable', '');
+        } else {
+          this._element[0].removeAttribute('draggable');
+        }
+      },
+
+      /**
+       * @return {Boolean}
+       */
+      isDraggable: function() {
+        return this._element[0].hasAttribute('draggable');
+      },
+
+      /**
+       * @param {Number} index
+       * @param {Object} [options]
+       * @param {Function} [options.callback]
+       * @param {String} [options.animation]
+       */
+      setActiveCarouselItemIndex: function(index, options) {
+        options = options || {};
+
+        index = Math.max(0, Math.min(index, this._getCarouselItemCount() - 1));
+        var scroll = this._getCarouselItemSize() * index;
+        var max = this._calculateMaxScroll();
+
+        this._scroll = Math.max(0, Math.min(max, scroll));
+        this._scrollTo(this._scroll, options.animation !== 'none');
+      },
+
+      /**
+       * @return {Number}
+       */
+      getActiveCarouselItemIndex: function() {
+        var scroll = this._scroll;
+        var count = this._getCarouselItemCount();
+        var size = this._getCarouselItemSize();
+
+        for (var i = 0; i < count; i++) {
+          if (size * i <= scroll && size * (i + 1) > scroll) {
+            return i;
+          }
+        }
+        throw new Error('invalid state');
+      },
+
+      /**
+       * @param {Object} [options]
+       * @param {Function} [options.callback]
+       * @param {String} [options.animation]
+       */
+      next: function(options) {
+        this.setActiveCarouselItemIndex(this.getActiveCarouselItemIndex() + 1, options);
+      },
+
+      /**
+       * @param {Object} [options]
+       * @param {Function} [options.callback]
+       * @param {String} [options.animation]
+       */
+      prev: function(options) {
+        this.setActiveCarouselItemIndex(this.getActiveCarouselItemIndex() - 1, options);
+      },
+
+      /**
+       * @param {Boolean} enabled
+       */
+      setAutoScrollEnabled: function(enabled) {
+        if (enabled) {
+          this._element[0].setAttribute('autoscroll', '');
+        } else {
+          this._element[0].removeAttribute('autoscroll');
+        }
+      },
+
+      /**
+       * @param {Boolean} enabled
+       */
+      isAutoScrollEnabled: function(enabled) {
+        return this._element[0].hasAttribute('autoscroll');
+      },
+
+      /**
+       * @param {Boolean} disabled
+       */
+      setDisabled: function(disabled) {
+        if (disabled) {
+          this._element[0].setAttribute('disabled', '');
+        } else {
+          this._element[0].removeAttribute('disabled');
+        }
+      },
+
+      /**
+       * @return {Boolean}
+       */
+      isDisabled: function() {
+        return this._element[0].hasAttribute('disabled');
+      },
+
+      /**
+       * @param {Boolean} scrollable
+       */
+      setOverscrollable: function(scrollable) {
+        if (scrollable) {
+          this._element[0].setAttribute('overscrollable', '');
+        } else {
+          this._element[0].removeAttribute('overscrollable');
+        }
+      },
+
+      /**
+       * @param {Object} trait
+       */
+      _mixin: function(trait) {
+        Object.keys(trait).forEach(function(key) {
+          this[key] = trait[key];
+        }.bind(this));
+      },
+
+      /**
+       * @return {Boolean}
+       */
+      _isVertical: function() {
+        return this._element.attr('direction') === 'vertical';
       },
 
       _prepareEventListeners: function() {
         this._hammer = new Hammer(this._element[0]);
 
-        var scrollTo = function() {
-        }.bind(this);
+        this._hammer.on('drag', this._bindedOnDrag);
+        this._hammer.on('dragend', this._bindedOnDragEnd);
+      },
 
-        this._hammer.on('drag', function(event) {
-          var scroll = this._scroll - event.gesture.deltaX;
-          this._scrollTo(scroll);
-        }.bind(this));
+      _onDrag: function(event) {
+        this._lastDragEvent = event;
 
-        this._hammer.on('dragend', function(event) {
-          this._scroll = this._scroll - event.gesture.deltaX;
-          if (this._isOverScroll(this._scroll)) {
-            this._scrollToKillOverScroll();
-          } else {
-            this._startMomemtumScroll(event);
-          }
-        }.bind(this));
+        var scroll = this._scroll - this._getScrollDelta(event);
+        this._scrollTo(scroll);
+        event.gesture.preventDefault();
+      },
+
+      _onDragEnd: function(event) {
+        this._scroll = this._scroll - this._getScrollDelta(event);
+        if (this._isOverScroll(this._scroll)) {
+          this._scrollToKillOverScroll();
+        } else if (this._lastDragEvent !== null) {
+          this._startMomemtumScroll(event);
+        }
+        this._lastDragEvent = null;
+        event.gesture.preventDefault();
+      },
+
+      /**
+       * @return {Boolean}
+       */
+      isOverscrollable: function() {
+        return this._element[0].hasAttribute('overscrollable');
       },
 
       _startMomemtumScroll: function(event) {
-        var velocity = event.gesture.velocityX;
-        var duration = 0.3;
-        var scrollDelta = duration * 1000 * velocity;
-        console.log(scrollDelta);
-        var scroll = this._scroll + (event.gesture.deltaX > 0 ? -scrollDelta : scrollDelta);
-        scroll = this._normalizeScrollPosition(scroll);
+        if (this._lastDragEvent !== null) {
+          var velocity = this._getScrollVelocity(this._lastDragEvent);
+          var duration = 0.3;
+          var scrollDelta = duration * 100 * velocity;
+          var scroll = this._scroll + (this._getScrollDelta(this._lastDragEvent) > 0 ? -scrollDelta : scrollDelta);
+          scroll = this._normalizeScrollPosition(scroll);
 
-        this._scroll = Math.min(Math.max(scroll, 0), this._calculateMaxScroll());
+          this._scroll = scroll;
 
-        animit(this._getItemElements())
-          .queue({
-            transform: 'translate3d(' + -this._scroll + 'px, 0px, 0px)'
-          }, {
-            duration: duration,
-            timing: 'cubic-bezier(.1, .7, .1, 1)'
-          })
-          .play();
+          animit(this._getCarouselItemElements())
+            .queue({
+              transform: this._generateScrollTransform(this._scroll)
+            }, {
+              duration: duration,
+              timing: 'cubic-bezier(.1, .7, .1, 1)'
+            })
+            .play();
+        }
       },
 
       _normalizeScrollPosition: function(scroll) {
-        var arr = [];
-        var width = this._element[0].getBoundingClientRect().width;
-        for (var i = 0; i < this._getItemCount(); i++) {
-          arr.push(i * width);
+        var max = this._calculateMaxScroll();
+
+        if (this.isAutoScrollEnabled()) {
+          var arr = [];
+          var size = this._getCarouselItemSize();
+
+          for (var i = 0; i < this._getCarouselItemCount(); i++) {
+            if (max >= i * size) { 
+              arr.push(i * size);
+            }
+          }
+          arr.push(max);
+
+          arr.sort(function(left, right) {
+            left = Math.abs(left - scroll);
+            right = Math.abs(right - scroll);
+
+            return left - right;
+          });
+
+          return arr[0];
+        } else {
+          return Math.max(0, Math.min(max, scroll));
         }
-        arr.sort(function(left, right) {
-          left = Math.abs(left - scroll);
-          right = Math.abs(right - scroll);
-
-          return left - right;
-        });
-
-        return arr[0];
       },
 
-      _updateLayout: function() {
-        //...
-      },
-
-      _getItemElements: function() {
+      /**
+       * @return {Array}
+       */
+      _getCarouselItemElements: function() {
         var items = [];
         var children = this._element.children();
 
         for (var i = 0; i < children.length; i++) {
-          items.push(children[i]);
+          if (children[i].nodeName.toLowerCase() === 'ons-carousel-item') {
+            items.push(children[i]);
+          }
         }
 
         return items;
       },
 
-      _scrollTo: function(scroll) {
-        var items = [];
+      /**
+       * @param {Number} scroll
+       * @param {Boolean} animate
+       */
+      _scrollTo: function(scroll, animate) {
         var self = this;
-        var children = this._element.children();
+        var isOverscrollable = this.isOverscrollable();
 
-        for (var i = 0; i < children.length; i++) {
-          items.push(children[i]);
+        if (animate) {
+          animit(this._getCarouselItemElements())
+            .queue({
+              transform: this._generateScrollTransform(normalizeScroll(scroll))
+            }, {
+              duration: 0.3,
+              timing: 'cubic-bezier(.1, .7, .1, 1)'
+            })
+            .play();
+        } else {
+          animit(this._getCarouselItemElements())
+            .queue({
+              transform: this._generateScrollTransform(normalizeScroll(scroll))
+            })
+            .play();
         }
 
-        animit(items)
-          .queue({transform: 'translate3d(' + -normalizeScroll(scroll) + 'px, 0px, 0px)'})
-          .play();
-
         function normalizeScroll(scroll) {
-          var ratio = 0.2;
+          var ratio = 0.35;
 
           if (scroll < 0) {
-            return Math.round(scroll * ratio);
+            return isOverscrollable ? Math.round(scroll * ratio) : 0;
           }
 
           var maxScroll = self._calculateMaxScroll();
           if (maxScroll < scroll) {
-            return maxScroll + Math.round((scroll - maxScroll) * ratio);
+            return isOverscrollable ? maxScroll + Math.round((scroll - maxScroll) * ratio) : maxScroll;
           }
 
           return scroll;
@@ -181,7 +493,7 @@ limitations under the License.
       },
 
       _calculateMaxScroll: function() {
-        return (this._getItemCount() - 1) * this._element[0].getBoundingClientRect().width;
+        return this._getCarouselItemCount() * this._getCarouselItemSize() - this._getElementSize();
       },
 
       _isOverScroll: function(scroll) {
@@ -192,27 +504,30 @@ limitations under the License.
       },
 
       _scrollToKillOverScroll: function() {
+        var duration = 0.4;
+        
         if (this._scroll < 0) {
-          animit(this._getItemElements())
+          animit(this._getCarouselItemElements())
             .queue({
-              transform: 'translate3d(0px, 0px, 0px)'
+              transform: this._generateScrollTransform(0)
             }, {
-              duration: 0.1,
-              timing: 'cubic-bezier(.1, .7, .1, 1)'
+              duration: duration,
+              timing: 'cubic-bezier(.1, .4, .1, 1)'
             })
             .play();
           this._scroll = 0;
           return;
         }
 
-        var maxScroll = (this._getItemCount() - 1) * this._element[0].getBoundingClientRect().width;
+        var maxScroll = this._calculateMaxScroll();
+
         if (maxScroll < this._scroll) {
-          animit(this._getItemElements())
+          animit(this._getCarouselItemElements())
             .queue({
-              transform: 'translate3d(' + -maxScroll + 'px, 0px, 0px)'
+              transform: this._generateScrollTransform(maxScroll)
             }, {
-              duration: 0.1,
-              timing: 'cubic-bezier(.1, .7, .1, 1)'
+              duration: duration,
+              timing: 'cubic-bezier(.1, .4, .1, 1)'
             })
             .play();
           this._scroll = maxScroll;
@@ -222,21 +537,30 @@ limitations under the License.
         return;
       },
 
-      _getItemCount: function() {
-        return this._element.children().length;
+      /**
+       * @return {Number}
+       */
+      _getCarouselItemCount: function() {
+        return this._getCarouselItemElements().length;
       },
 
       /**
-       *
+       * Refresh carousel item layout.
        */
       refresh: function() {
-
+        this._mixin(this._isVertical() ? VerticalModeTrait : HorizontalModeTrait);
+        this._layoutCarouselItems();
       },
 
       _destroy: function() {
         this.emit('destroy', {navigator: this});
-      }
 
+        this._hammer.off('drag', this._bindedOnDrag);
+        this._hammer.off('dragend', this._bindedOnDragEnd);
+
+        // Remove event listeners
+        this._element = this._scope = this._attrs = null;
+      }
     });
 
     MicroEvent.mixin(CarouselView);
