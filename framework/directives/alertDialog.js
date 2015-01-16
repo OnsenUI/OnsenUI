ad8a80ab8251cbf99f7711efc38da142f135bbbb
Mitsunori Kubota <mitsunori@asial.co.jp>
2015-01-16 19:03:57 +0900

    Updated doccomments for new style.

--- framework/directives/alertDialog.js
+++ framework/directives/alertDialog.js
@@ -1,55 +1,11 @@
+
 /**
  * @ngdoc directive
  * @id alert-dialog 
  * @name ons-alert-dialog
  * @description 
- *  [en]Alert dialog that is displayed on top of the current screen.[/en]
- *  [ja]現在のスクリーンにアラートダイアログを表示します。[/ja]
- * @param var 
- *  [en]Variable name to refer this alert dialog.[/en]
- *  [ja]このアラートダイアログを参照するための名前を指定します。[/ja]
- * @param modifier
- *  [en]The appearance of the dialog.[/en]
- *  [ja]ダイアログの表現を指定します。[/ja]
- * @param cancelable
- *  [en]If this attribute is set the dialog can be closed by tapping the background or by pressing the back button.[/en] 
- *  [ja]この属性があると、ダイアログが表示された時に、背景やバックボタンをタップした時にダイアログを閉じます。[/ja]
- * @param disabled
- *  [en]If this attribute is set the dialog is disabled.[/en]
- *  [ja]この属性がある時、アラートダイアログはdisabled状態になります。[/ja]
- * @param animation
- *  [en]The animation used when showing and hiding the dialog. Can be either "none" or "default".[/en]
- *  [ja]ダイアログを表示する際のアニメーション名を指定します。[/ja]
- * @param mask-color
- *  [en]Color of the background mask. Default is "rgba(0, 0, 0, 0.2)".[/en]
- *  [ja]背景のマスクの色を指定します。デフォルトは"rgba(0, 0, 0, 0.2)"です。[/ja]
- * @property show(options)
- *  [en]Show the alert dialog.[/en]
- *  [ja]ダイアログを開きます。[/ja]
- * @property hide(options)
- *  [en]Hide the alert dialog.[/en]
- *  [ja]ダイアログを閉じます。[/ja]
- * @property isShown()
- *  [en]Returns whether the dialog is visible or not.[/en]
- *  [ja]ダイアログが表示されているかどうかを返します。[/ja]
- * @property destroy()
- *  [en]Destroy the alert dialog and remove it from the DOM tree.[/en]
- *  [ja]ダイアログを破棄して、DOMツリーから取り除きます。[/ja]
- * @property setCancelable(cancelable)
- *  [en]Define whether the dialog can be canceled by the user or not.[/en]
- *  [ja]アラートダイアログを表示した際に、ユーザがそのダイアログをキャンセルできるかどうかを指定します。[/ja]
- * @property isCancelable()
- *  [en]Returns whether the dialog is cancelable or not.[/en]
- *  [ja]このアラートダイアログがキャンセル可能かどうかを返します。[/ja]
- * @property setDisabled(disabled)
- *  [en]Disable or enable the alert dialog.[/en]
- *  [ja]このアラートダイアログをdisabled状態にするかどうかを設定します。[/ja]
- * @property isDisabled()
- *  [en]Returns whether the dialog is disabled or enabled.[/en]
- *  [ja]このアラートダイアログがdisabled状態かどうかを返します。[/ja]
- * @property on(eventName,listener)
- *  [en]Add an event listener. Preset events are preshow, postshow, prehide and posthide.[/en]
- *  [ja]イベントリスナーを追加します。preshow, postshow, prehide, posthideを指定できます。[/ja]
+ * [en]Alert dialog that is displayed on top of the current screen.[/en]
+ * [ja]現在のスクリーンにアラートダイアログを表示します。[/ja]
  * @codepen Qwwxyp
  * @example
  * <script>
@@ -71,6 +27,184 @@
  *     </div>
  *   </ons-alert-dialog>  
  * </script>
+ */
+
+/**
+ * @ngdoc event
+ * @name preshow
+ * @description
+ * [en][/en]
+ * [ja]アラートダイアログが表示される直前に発火します。[/ja]
+ * @param {Object} event [en]Event object.[/en]
+ * @param {Object} event.alertDialog
+ * @param {Function} event.cancel 
+ */
+
+/**
+ * @ngdoc event
+ * @name postshow
+ * @description
+ * [en][/en]
+ * [ja]アラートダイアログが表示された直後に発火します。[/ja]
+ * @param {Object} event [en]Event object.[/en]
+ * @param {Object} event.alertDialog
+ */
+
+/**
+ * @ngdoc event
+ * @name prehide
+ * @description
+ * [en][/en]
+ * [ja]アラートダイアログが隠れる直前に発火します。[/ja]
+ * @param {Object} event [en]Event object.[/en]
+ * @param {Object} event.alertDialog
+ * @param {Function} event.cancel 
+ */
+
+/**
+ * @ngdoc event
+ * @name posthide
+ * @description
+ * [en][/en]
+ * [ja]アラートダイアログが隠れた後に発火します。[/ja]
+ * @param {Object} event [en]Event object.[/en]
+ * @param {Object} event.alertDialog
+ */
+
+
+/**
+ * @ngdoc attribute
+ * @name var
+ * @type {String}
+ * @description
+ *  [en]Variable name to refer this alert dialog.[/en]
+ *  [ja]このアラートダイアログを参照するための名前を指定します。[/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name modifier
+ * @type {String}
+ * @description
+ *  [en]The appearance of the dialog.[/en]
+ *  [ja]ダイアログの表現を指定します。[/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name cancelable
+ * @description
+ *  [en]If this attribute is set the dialog can be closed by tapping the background or by pressing the back button.[/en] 
+ *  [ja]この属性があると、ダイアログが表示された時に、背景やバックボタンをタップした時にダイアログを閉じます。[/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name disabled
+ * @description
+ *  [en]If this attribute is set the dialog is disabled.[/en]
+ *  [ja]この属性がある時、アラートダイアログはdisabled状態になります。[/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name animation
+ * @type {String}
+ * @default default
+ * @description
+ *  [en]The animation used when showing and hiding the dialog. Can be either "none" or "default".[/en]
+ *  [ja]ダイアログを表示する際のアニメーション名を指定します。デフォルトでは"none"か"default"が指定できます。[/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name mask-color
+ * @type {String}
+ * @default rgba(0, 0, 0, 0.2)
+ * @description
+ *  [en]Color of the background mask. Default is "rgba(0, 0, 0, 0.2)".[/en]
+ *  [ja]背景のマスクの色を指定します。デフォルトは"rgba(0, 0, 0, 0.2)"です。[/ja]
+ */
+
+/**
+ * @ngdoc method
+ * @signature show([options])
+ * @description
+ *  [en]Show the alert dialog.[/en]
+ *  [ja]ダイアログを開きます。[/ja]
+ * @param {Object} options
+ */
+
+/**
+ * @ngdoc method
+ * @signature hide([options])
+ * @description
+ *  [en]Hide the alert dialog.[/en]
+ *  [ja]ダイアログを閉じます。[/ja]
+ * @param {Object} options
+ */
+
+/**
+ * @ngdoc method
+ * @signature isShown()
+ * @description
+ *  [en]Returns whether the dialog is visible or not.[/en]
+ *  [ja]ダイアログが表示されているかどうかを返します。[/ja]
+ * @return {Boolean}
+ */
+
+/**
+ * @ngdoc method
+ * @signature destroy()
+ * @description
+ *  [en]Destroy the alert dialog and remove it from the DOM tree.[/en]
+ *  [ja]ダイアログを破棄して、DOMツリーから取り除きます。[/ja]
+ */
+
+/**
+ * @ngdoc method
+ * @signature setCancelable(cancelable)
+ * @description
+ *  [en]Define whether the dialog can be canceled by the user or not.[/en]
+ *  [ja]アラートダイアログを表示した際に、ユーザがそのダイアログをキャンセルできるかどうかを指定します。[/ja]
+ * @param {Boolean} cancelable
+ */
+
+/**
+ * @ngdoc method
+ * @signature isCancelable()
+ * @description
+ *  [en]Returns whether the dialog is cancelable or not.[/en]
+ *  [ja]このアラートダイアログがキャンセル可能かどうかを返します。[/ja]
+ * @return {Boolean}
+ */
+
+/**
+ * @ngdoc method
+ * @signature setDisabled(disabled)
+ * @description
+ *  [en]Disable or enable the alert dialog.[/en]
+ *  [ja]このアラートダイアログをdisabled状態にするかどうかを設定します。[/ja]
+ * @param {Boolean} disabled
+ */
+
+/**
+ * @ngdoc method
+ * @signature isDisabled()
+ * @description
+ *  [en]Returns whether the dialog is disabled or enabled.[/en]
+ *  [ja]このアラートダイアログがdisabled状態かどうかを返します。[/ja]
+ * @return {Boolean}
+ */
+
+/**
+ * @ngdoc method
+ * @signature on(eventName, listener)
+ * @description
+ *  [en]Add an event listener. Preset events are preshow, postshow, prehide and posthide.[/en]
+ *  [ja]イベントリスナーを追加します。preshow, postshow, prehide, posthideを指定できます。[/ja]
+ * @param {String} eventName
+ * @param {Function} listener
  */
 
 (function() {
