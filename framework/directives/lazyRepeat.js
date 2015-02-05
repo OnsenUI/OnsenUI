fcad44a9a07f4d731abf4a92377ba01454b3e0a5
Andreas Argelius <andreas@argeli.us>
2015-02-05 11:43:19 +0900

    Implemented <ons-lazy-repeat> component.

--- framework/directives/lazyRepeat.js
+++ framework/directives/lazyRepeat.js
@@ -3,28 +3,65 @@
  * @id lazy-repeat 
  * @name ons-lazy-repeat
  * @description 
- *  [en][/en]
+ *  [en]Using this component a list with millions of items can be rendered without a drop in performance. It does that by "lazily" loading elements into the DOM when they come into view and removing items from the DOM when they are not visible.[/en]
  *  [ja][/ja]
+ * @param ons-lazy-repeat
+ *  [en]A delegate object, can be either an object attached to the scope (when using AngularJS) or a normal JavaScript variable.[/en]
+ *  [ja][/ja]
+ * @example
+ * <script>
+ *   ons.bootstrap()
+ *
+ *   .controller('MyController', function($scope) {
+ *     $scope.MyDelegate = {
+ *       countItems: function() {
+ *         // Return number of items.
+ *         return 1000000;
+ *       },
+ *
+ *       calculateItemHeight: function(index) {
+ *         // Return the height of an item in pixels.
+ *         return 45;
+ *       },
+ *
+ *       configureItemScope: function(index, itemScope) {
+ *         // Initialize scope
+ *         itemScope.item = 'Item #' + (index + 1);
+ *       },
+ *
+ *       destroyItemScope: function(index, itemScope) {
+ *         // Optional method that is called when an item is unloaded.
+ *         console.log('Destroyed item with index: ' + index);
+ *       }
+ *     };
+ *   });
+ * </script>
+ *
+ * <ons-list ng-controller="MyController">
+ *   <ons-list-item ons-lazy-repeat="MyDelegate"`>
+ *     {{ item }}
+ *   </ons-list-item>
+ * </ons-list>
  */
-
 (function() {
   'use strict';
 
   var module = angular.module('onsen');
 
   /**
-   * Dialog directive.
+   * Lazy repeat directive.
    */
   module.directive('onsLazyRepeat', function($onsen, LazyRepeatView) {
     return {
       restrict: 'A',
       replace: false,
-      compile: function(element, attrs) {
+      transclude: 'element',
+      compile: function(element, attrs, linker) {
         return function(scope, element, attrs) {
-          var lazyRepeat = new LazyRepeatView(scope, element, attrs);     
+          var lazyRepeat = new LazyRepeatView(scope, element, attrs, linker);     
 
           scope.$on('$destroy', function() {
-            scope = element = attrs = null;
+            scope = element = attrs = linker = null;
           });
         };
       }
--- framework/views/lazyRepeat.js
+++ framework/views/lazyRepeat.js
@@ -19,7 +19,7 @@ limitations under the License.
   'use strict';
   var module = angular.module('onsen');
 
-  module.factory('LazyRepeatView', function($onsen) {
+  module.factory('LazyRepeatView', function($onsen, $document, $compile) {
 
     var LazyRepeatView = Class.extend({
 
@@ -28,42 +28,235 @@ limitations under the License.
        * @param {jqLite} element
        * @param {Object} attrs
        */
-      init: function(scope, element, attrs) {
+      init: function(scope, element, attrs, linker) {
         this._element = element;
         this._scope = scope;
         this._attrs = attrs;
+        this._linker = linker;
 
+        this._parentElement = element.parent();
+        this._itemHeightSum = [];
+        this._maxIndex = 0;
+
+        this._delegate = this._getDelegate();
+
+        this._renderedElements = {};
         this._addEventListeners();
 
+        this._scope.$watch(
+          function() {
+            return this._countItems();
+          }.bind(this),
+          function() {
+            this._render();
+          }.bind(this)
+        );
+
         this._scope.$on('$destroy', this._destroy.bind(this));
+        this._render();
       },
 
       _getDelegate: function() {
         var delegate = this._scope.$eval(this._attrs.onsLazyRepeat);
 
         if (typeof delegate === 'undefined') {
+          /*jshint evil:true */
           delegate = eval(this._attrs.onsLazyRepeat);
         }
 
         return delegate;
       },
 
+      _countItems: function() {
+        return this._delegate.countItems();
+      },
+
+      _getItemHeight: function(i) {
+        return this._delegate.calculateItemHeight(i);
+      },
+      
+      _getTopOffset: function() {
+        return this._parentElement[0].getBoundingClientRect().top;
+      },
+
+      _render: function() {
+        var items = this._getItemsInView(),
+          keep = {};
+
+        this._parentElement.css('height', this._itemHeightSum[this._maxIndex] + 'px');
+
+        for (var i = 0, l = items.length; i < l; i ++) {
+          var _item = items[i];
+          this._renderElement(_item);
+          keep[_item.index] = true;
+        }
+
+        for (var key in this._renderedElements) {
+          if (this._renderedElements.hasOwnProperty(key) && !keep.hasOwnProperty(key)) {
+            this._removeElement(key);
+          }
+        }
+      },
+
+      _isRendered: function(i) {
+        return this._renderedElements.hasOwnProperty(i);
+      },
+      
+      _renderElement: function(item) {
+        if (this._isRendered(item.index)) {
+          return;
+        }
+
+        var childScope = this._scope.$new();
+        this._addSpecialProperties(item.index, childScope);
+
+        this._linker(childScope, function(clone) {
+          if (this._delegate.configureItemScope) {
+            this._delegate.configureItemScope(item.index, childScope);
+          }
+          else if (this._delegate.createItemContent) {
+            clone.append(this._delegate.createItemContent(item.index));
+          }
+
+          this._parentElement.append(clone);
+          clone.removeAttr('ons-lazy-repeat');
+
+          clone.css({
+            position: 'absolute',
+            top: item.top + 'px',
+            left: '0px',
+            right: '0px',
+            display: 'none'
+          });
+
+          var element = {
+            element: clone,
+            scope: childScope
+          };
+
+          $compile(clone)(childScope);
+ 
+          // Don't show elements before they are finished rendering.
+          this._scope.$evalAsync(function() {
+            clone.css('display', 'block');
+          });
+
+          this._renderedElements[item.index] = element;
+        }.bind(this));
+      },
+
+      _removeElement: function(i) {
+        if (!this._isRendered(i)) {
+          return;
+        }
+
+        var element = this._renderedElements[i];
+
+        if (this._delegate.destroyItemScope) {
+          this._delegate.destroyItemScope(i, element.scope);
+        }
+        else if (this._delegate.destroyItemContent) {
+          this._delegate.destroyItemContent(i, element.element.children()[0]);
+        }
+
+        element.element.remove();
+        element.scope.$destroy();
+
+        delete this._renderedElements[i];
+      },
+
+      _calculateStartIndex: function(current) {
+        var start = 0,
+          end = this._maxIndex;
+
+        // Binary search for index at top of screen so
+        // we can speed up rendering.
+        while (true) {
+          var middle = Math.floor((start + end) / 2),
+            value = current + this._itemHeightSum[middle];
+
+          if (end < start) {
+            return 0;
+          }
+          else if (value >= 0 && value - this._getItemHeight(middle) < 0) {
+            return middle;
+          }
+          else if (isNaN(value) || value >= 0) {
+            end = middle - 1;
+          }
+          else {
+            start = middle + 1;
+          }
+
+        }
+      },
+
+      _getItemsInView: function() {
+        var topOffset = this._getTopOffset(),
+          topPosition = topOffset,
+          cnt = this._countItems();
+
+        var startIndex = this._calculateStartIndex(topPosition);
+        startIndex = Math.max(startIndex - 10, 0);
+
+        if (startIndex > 0) {
+          topPosition += this._itemHeightSum[startIndex - 1];
+        }
+
+        var items = [];
+        for (var i = startIndex; i < cnt && topPosition < 2 * window.innerHeight; i++) {
+          var h = this._getItemHeight();
+
+          if (i >= this._itemHeightSum.length) {
+            this._itemHeightSum = this._itemHeightSum.concat(new Array(100));
+          }
+
+          if (i > 0) {
+            this._itemHeightSum[i] = this._itemHeightSum[i - 1] + h;
+          }
+          else {
+            this._itemHeightSum[i] = h;
+          }
+
+          this._maxIndex = Math.max(i, this._maxIndex);
+
+          items.push({
+            index: i,
+            top: topPosition - topOffset
+          });
+
+          topPosition += h;
+        }
+
+        return items;
+      },
+
+      _addSpecialProperties: function(i, scope) {
+        scope.$index = i;
+        scope.$first = i === 0;
+        scope.$last = i === this._countItems() - 1;
+        scope.$middle = !scope.$first && !scope.$last;
+        scope.$even = i % 2 === 0;
+        scope.$odd = !scope.$even;
+      },
+
+      _onScroll: function() {
+        this._render();
+      },
+
       _addEventListeners: function() {
         this._bindedOnScroll = this._onScroll.bind(this); 
-  
-        this._element.parent().on('scroll', this._bindedOnScroll);
+        $document[0].addEventListener('scroll', this._bindedOnScroll, true);
       },
 
       _removeEventListeners: function() {
-        this._element.parent().off('scroll', this._bindedOnScroll);
+        $document[0].removeEventListener('scroll', this._bindedOnScroll, true);
       },
       
       _destroy: function() {
         this._removeEventListeners(); 
-
         this._element = this._scope = this._attrs = null;
       }
-
     });
 
     return LazyRepeatView;
