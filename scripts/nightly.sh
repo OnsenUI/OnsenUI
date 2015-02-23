332a741ab7bbc3b808de1be6c7770c4b63b81fe1
Andreas Argelius <andreas@argeli.us>
2015-02-23 18:55:02 +0900

    Added automatic build using Circle CI.

---
+++ scripts/nightly.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 3 ] || die "usage: $0 build_number commit_hash destination_dir"
+
+DIR=$(dirname $0)
+
+source $DIR/utils.inc
+
+BUILD_NUMBER=$1
+COMMIT_HASH=$2
+VERSION=$(getVersion $DIR/../package.json)
+DATE=`date +%Y%m%d`
+DESTINATION_DIR=$3
+
+# Remove version suffix.
+VERSION=${VERSION%-*}
+
+BUILD_VERSION="$VERSION-build.$BUILD_NUMBER"
+
+$DIR/bump.sh $BUILD_VERSION
+
+pushd $DIR/../css-components
+gulp build
+popd
+
+pushd $DIR/../
+gulp build
+popd
+
+mv $DIR/../project_templates/onsen_master_detail.zip $DESTINATION_DIR/onsenui-$BUILD_VERSION.$BUILD_NUMBER_$DATE+$COMMIT_HASH.zip
