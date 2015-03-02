610c03e10133233c65922652cdfdce10243277de
Andreas Argelius <andreas@argeli.us>
2015-03-02 20:12:32 +0900

    Updated copyright year.

--- framework/views/dialogAnimator.js
+++ framework/views/dialogAnimator.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2013-2014 ASIAL CORPORATION
+Copyright 2013-2015 ASIAL CORPORATION
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
