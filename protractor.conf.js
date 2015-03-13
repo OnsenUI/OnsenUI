5b73f65a1d75dd92712e4a00bca312751189fdc8
Andreas Argelius <andreas@argeli.us>
2015-03-13 12:12:43 +0900

    Updated protractor conf.

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,8 +2,6 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
-  seleniumServerJar: './.selenium/selenium-server-standalone-2.45.0.jar',  
-  chromeDriver: './.selenium/chromedriver',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
