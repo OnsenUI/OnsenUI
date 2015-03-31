07eb63e8a86b154dab379e935b6aaa8705f34988
Ryohei Ikegami <ikegami@asial.co.jp>
2015-03-31 20:08:52 +0900

    Add AnimationChooser

---
+++ framework/views/animationChooser.js
@@ -0,0 +1,70 @@
+/*
+Copyright 2013-2015 ASIAL CORPORATION
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+(function() {
+  'use strict';
+
+  var module = angular.module('onsen');
+
+  module.factory('AnimationChooser', function() {
+    var AnimationChooser = Class.extend({
+      init: function(opts) {
+        this._animators = opts.animators;
+        this._baseClass = opts.baseClass;
+        this._baseClassName = opts.baseClassName || opts.baseClass.name;
+        this._animation = opts.defaultAnimation || 'default';
+        this._animationOptions = opts.defaultAnimationOptions || {};
+
+        if (!this._animators[this._animation]) {
+          throw new Error('No such animation: ' + this._animation);
+        }
+      },
+
+      newAnimator: function(options, defaultAnimator) {
+        options = options || {};
+
+        var animator = null;
+
+        if (options.animation instanceof this._baseClass) {
+          return options.animation;
+        }
+
+        var Animator = null;
+
+        if (typeof options.animation === 'string') {
+          Animator = this._animators[options.animation];
+        }
+        Animator = Animator || this._animators[this._animation];
+
+        if (!Animator && defaultAnimator) {
+          animator = defaultAnimator;
+        } else {
+          var animationOpts = angular.extend({}, this._animationOptions, options.animationrOptions || {});
+          animator = new Animator(animationOpts);
+        }
+
+        if (!(animator instanceof this._baseClass)) {
+          throw new Error('"animator" is not an instance of ' + this._baseClassName + '.');
+        }
+
+        return animator;
+      }
+    });
+    return AnimationChooser;
+  });
+
+})();
