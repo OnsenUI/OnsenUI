26551463302b87568c68e7a9682b9bb94593dccb
Andreas Argelius <andreas@argeli.us>
2015-04-02 18:45:27 +0900

    Fixed broken split view.

---
+++ test/e2e/composition/scenarios.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  describe('navigator with sliding menu', function() {
+    var path = '/test/e2e/composition/navigator+splitView.html',
+      EC = protractor.ExpectedConditions;
+
+    beforeEach(function() {
+      browser.get(path);
+      browser.waitForAngular();
+    });
+
+    it('should push and pop the page', function() {
+      var pushPage = element(by.id('push-page')),
+        popPage = element(by.id('pop-page'));
+
+      pushPage.click();
+      browser.wait(EC.visibilityOf(popPage));
+      expect(popPage.isDisplayed()).toBeTruthy();
+      expect(pushPage.isDisplayed()).not.toBeTruthy();
+
+      popPage.click();
+      browser.wait(EC.visibilityOf(pushPage));
+      browser.wait(EC.stalenessOf(popPage));
+      expect(popPage.isPresent()).not.toBeTruthy();
+      expect(pushPage.isDisplayed()).toBeTruthy();
+    });
+  });
+})();
