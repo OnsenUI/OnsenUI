ba6346a03de0951d85fc5cda2cbd02c223e04796
andipavllo <andi.pavllo@gmail.com>
2015-05-25 14:58:07 +0900

    added ons-button karma test

---
+++ core/elements/ons-button.spec.js
@@ -0,0 +1,23 @@
+
+describe('ons-button', function() {
+  it('provide \'OnsButtonElement\' global variable', function() {
+    expect(!!window.OnsButtonElement).to.be.true;
+  });
+
+  it('provide modifier attribute', function() {
+    var element = new OnsButtonElement();
+    element.setAttribute('modifier', 'hoge');
+    expect(element.classList.contains('button--hoge')).to.be.true;
+
+    element.setAttribute('modifier', ' foo bar');
+    expect(element.classList.contains('button--foo')).to.be.true;
+    expect(element.classList.contains('button--bar')).to.be.true;
+    expect(!element.classList.contains('button--hoge')).to.be.true;
+
+    element.classList.add('button--piyo');
+    element.setAttribute('modifier', 'fuga');
+    expect(element.classList.contains('button--piyo')).to.be.true;
+    expect(element.classList.contains('button--fuga')).to.be.true;
+  });
+});
+
