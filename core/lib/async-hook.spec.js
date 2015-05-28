53b08da429de7156067a8d4a2aa65c15aebddf88
anatoo <anatoo.jp@gmail.com>
2015-05-28 18:28:08 +0900

    Added ons._internal.AsyncHook.

---
+++ core/lib/async-hook.spec.js
@@ -0,0 +1,66 @@
+
+describe('AsyncHook', function() {
+  it('provide \'ons._internal.AsyncHook\' global variable', function() {
+    expect(!!window.ons._internal.AsyncHook).to.equal(true);
+  });
+
+  describe('.addCallback() and .removeCallback()', function() {
+    var AsyncHook = ons._internal.AsyncHook;
+
+    it('should work on basic testcases', function() {
+      var hook = new AsyncHook();
+      var callback;
+
+      expect(hook._callbacks.length).to.equal(0);
+      hook.addCallback(callback = function(target, next) {
+        next(target);
+      });
+      expect(hook._callbacks.length).to.equal(1);
+      hook.removeCallback(callback);
+      expect(hook._callbacks.length).to.equal(0);
+    });
+  });
+
+  describe('.run()', function() {
+    var AsyncHook = ons._internal.AsyncHook;
+
+    it('should work on basic testcases', function(done) {
+      var hook = new AsyncHook();
+
+      hook.addCallback(function(next, target) {
+        next(target + target);
+      });
+      hook.addCallback(function(next, target) {
+        next(1 + target);
+      });
+      hook.run(function(result) {
+        expect(result).to.be.equal(5);
+        done();
+      }, 2);
+    });
+
+    it('should work on empty callbacks', function(done) {
+      var hook = new AsyncHook();
+
+      hook.run(function(result) {
+        expect(result).to.be.equal(2);
+        done();
+      }, 2);
+    });
+
+    it('should work on no target', function(done) {
+      var hook = new AsyncHook();
+
+      hook.addCallback(function(next) {
+        setTimeout(next, 20);
+      });
+      hook.addCallback(function(next) {
+        setTimeout(next, 10);
+      });
+      hook.run(function() {
+        done();
+      }, 2);
+    });
+  });
+});
+
