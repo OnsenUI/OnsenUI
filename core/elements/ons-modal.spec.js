b616bb9c399eb8246ec28ddb2f101fa3433ac308
andipavllo <andi.pavllo@gmail.com>
2015-06-01 10:55:25 +0900

    added ons-modal karma test

---
+++ core/elements/ons-modal.spec.js
@@ -0,0 +1,50 @@
+describe('ons-modal', function() {
+  it('provides \'OnsModalElement\' global variable', function() {
+    expect(window.OnsModalElement).to.be.ok;
+  });
+
+  it('classList contains \'modal\' by default', function() {
+  	var element = new OnsModalElement();
+  	expect(element.classList.contains('modal')).to.be.true;
+  });
+
+  it('children classList contains \'modal__content\' by default', function() {
+  	var element = new OnsModalElement();
+  	var wrapper = document.createElement('div');
+  	element.appendChild(wrapper);
+  	expect(element.children[0].classList.contains('modal__content')).to.be.true;
+  });
+
+  it('doesn\'t display anything by default', function() {
+  	var element = new OnsModalElement();
+  	expect(element.style.display).to.equal('none');
+  });
+
+  it('provides a \'show()\' method', function() {
+  	var element = new OnsModalElement();
+  	expect(element.style.display).to.equal('none');
+  	element.show();
+  	expect(element.style.display).to.equal('table');
+  });
+
+  it('provides a \'hide()\' method', function() {
+  	var element = new OnsModalElement();
+  	expect(element.style.display).to.equal('none');
+  	element.show();
+  	expect(element.style.display).to.equal('table');
+  	element.hide();
+  	expect(element.style.display).to.equal('none');
+  });
+
+  it('provides a \'toggle()\' method', function() {
+  	var element = new OnsModalElement();
+  	expect(element.style.display).to.equal('none');
+  	element.toggle();
+  	expect(element.style.display).to.equal('table');
+  	element.toggle();
+  	expect(element.style.display).to.equal('none');
+  	element.toggle();
+  	expect(element.style.display).to.equal('table');
+  });
+
+});
