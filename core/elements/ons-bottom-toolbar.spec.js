4d4f5ff7e6584bcd979ac3c234cad092dff5c248
Andi Pavllo <andi.pavllo@gmail.com>
2015-06-01 12:19:09 +0900

    fixed wrong indentation

--- core/elements/ons-bottom-toolbar.spec.js
+++ core/elements/ons-bottom-toolbar.spec.js
@@ -4,19 +4,19 @@ describe('ons-bottom-toolbar', function() {
   });
 
   it('classList contains \'bottom-bar\' by default', function() {
-  	var element = new OnsBottomToolbarElement();
-  	expect(element.classList.contains('bottom-bar')).to.be.true;
+    var element = new OnsBottomToolbarElement();
+    expect(element.classList.contains('bottom-bar')).to.be.true;
   });
 
   it('default \'position\' is \'absolute\' and default \'z-index\' is \'0\'', function() {
-  	var element = new OnsBottomToolbarElement();
-  	expect(element.style.zIndex).to.equal('0');
-  	expect(element.style.position).to.equal('absolute');
-  	expect(element.style.position).not.to.equal('relative');
+    var element = new OnsBottomToolbarElement();
+    expect(element.style.zIndex).to.equal('0');
+    expect(element.style.position).to.equal('absolute');
+    expect(element.style.position).not.to.equal('relative');
   });
 
   it('provides \'modifier\' attribute', function() {
-  	var element = new OnsBottomToolbarElement();
+    var element = new OnsBottomToolbarElement();
     element.setAttribute('modifier', 'hoge');
     expect(element.classList.contains('bottom-bar--hoge')).to.be.true;
 
@@ -32,7 +32,7 @@ describe('ons-bottom-toolbar', function() {
   });
 
   it('provides \'inline\' attribute', function() {
-  	var element = new OnsBottomToolbarElement();
+    var element = new OnsBottomToolbarElement();
     element.setAttribute('inline', '');
     expect(element.style.position).to.equal('static');
     expect(element.style.position).not.to.equal('absolute');
