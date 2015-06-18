59540c3b680d04938be18c521c07d571d6133477
andipavllo <andi.pavllo@gmail.com>
2015-06-18 17:38:06 +0900

    test(ons-tabbar): added karma test

---
+++ core/elements/ons-tabbar.spec.js
@@ -0,0 +1,128 @@
+describe('ons-tabbar', function() {
+  it('provides \'OnsTabbarElement\' global variable', function() {
+    expect(window.OnsTabbarElement).to.be.ok;
+  });
+
+  it('provides \'modifier\' attribute', function() {
+    var element = new OnsTabbarElement();
+    document.body.appendChild(element);
+    element.setAttribute('modifier', 'hoge');
+
+    expect(element.children[0].classList.contains('tab-bar--hoge__content')).to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--hoge')).to.be.true;
+
+    element.setAttribute('modifier', ' foo bar');
+    expect(element.children[0].classList.contains('tab-bar--foo__content')).to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--foo')).to.be.true;
+    expect(element.children[0].classList.contains('tab-bar--bar__content')).to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--bar')).to.be.true;
+    expect(element.children[0].classList.contains('tab-bar--hoge__content')).not.to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--hoge')).not.to.be.true;
+
+    element.children[0].classList.add('tab-bar--piyo');
+    element.children[1].classList.add('tab-bar--piyo');
+    element.setAttribute('modifier', 'fuga');
+    expect(element.children[0].classList.contains('tab-bar--piyo')).to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--piyo')).to.be.true;
+    expect(element.children[0].classList.contains('tab-bar--fuga__content')).to.be.true;
+    expect(element.children[1].classList.contains('tab-bar--fuga')).to.be.true;
+  });
+
+  it('accepts \'var\' attribute', function() {
+    var element = new OnsTabbarElement();
+
+    element.setAttribute('var', 'myVar');
+    expect((element).hasAttribute('var')).to.be.true;
+
+    expect(element.getAttribute('var')).to.equal('myVar');
+  });
+
+  it('has a unique auto generated id', function() {
+    var element = new OnsTabbarElement();
+    expect(element.getTabbarId()).to.be.ok;
+  });
+
+  it('has \'position\' property', function(done) {
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.innerHTML = '<ons-page><ons-tabbar id="top" position="top"></ons-tabbar>' + '<ons-tabbar id="bottom" position="bottom"></ons-tabbar></ons-page>';
+
+    var topElement = document.getElementById('top');
+    var bottomElement = document.getElementById('bottom');
+
+    setImmediate(function() {
+      expect(topElement.style.top).to.equal('0px');
+      expect(bottomElement.style.top).not.to.equal('0px');
+
+      expect(topElement._hasTopTabbar()).to.be.true;
+      expect(bottomElement._hasTopTabbar()).not.to.be.true;
+
+      expect(topElement.children[0].classList.contains('tab-bar--top__content')).to.be.true;
+      expect(bottomElement.children[0].classList.contains('tab-bar--top__content')).not.to.be.true;
+
+      expect((topElement.children[0]).hasAttribute('no-status-bar-fill')).to.be.true;
+      expect((bottomElement.children[0]).hasAttribute('no-status-bar-fill')).not.to.be.true;
+
+      expect(topElement.children[1].classList.contains('tab-bar--top')).to.be.true;
+      expect(bottomElement.children[1].classList.contains('tab-bar--top')).not.to.be.true;
+
+      done();
+    });
+  });
+
+  it('can be set visible or invisible', function() {
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.innerHTML = '<ons-tabbar id="top" position="top"></ons-tabbar>' + '<ons-tabbar id="bottom" position="bottom"></ons-tabbar>';
+
+    var topElement = document.getElementById('top');
+    var bottomElement = document.getElementById('bottom');
+
+    expect(topElement._getContentElement().style.top).to.equal('');
+    expect(topElement._getTabbarElement().style.display).to.equal('');
+    expect(bottomElement._getContentElement().style.bottom).to.equal('');
+    expect(bottomElement._getTabbarElement().style.display).to.equal('');
+
+    topElement.setTabbarVisibility(false);
+    bottomElement.setTabbarVisibility(false);
+    expect(topElement._getContentElement().style.top).to.equal('0px');
+    expect(topElement._getTabbarElement().style.display).to.equal('none');
+    expect(bottomElement._getContentElement().style.bottom).to.equal('0px');
+    expect(bottomElement._getTabbarElement().style.display).to.equal('none');
+
+    topElement.setTabbarVisibility(true);
+    bottomElement.setTabbarVisibility(true);
+    expect(topElement._getContentElement().style.top).to.equal('');
+    expect(topElement._getTabbarElement().style.display).to.equal('');
+    expect(bottomElement._getContentElement().style.bottom).to.equal('');
+    expect(bottomElement._getTabbarElement().style.display).to.equal('');
+  });
+
+  it('has accepts only \'ons-page\' as current page element', function() {
+    var element = new OnsTabbarElement();
+    var page = new OnsPageElement();
+    element._getContentElement().appendChild(page);
+    expect(element._getCurrentPageElement().classList.contains('page')).to.be.true;
+    expect(function(){element._getCurrentPageElement()}).not.to.throw('Invalid state: page element must be a "ons-page" element.');
+
+    element._getContentElement().removeChild(element._getContentElement().querySelector('ons-page'));
+    var button = new OnsButtonElement;
+    element._getContentElement().appendChild(button);
+    expect(function(){element._getCurrentPageElement()}).to.throw('Invalid state: page element must be a "ons-page" element.');
+  });
+
+  it('has two children by default', function() {
+    var element = new OnsTabbarElement();
+
+    expect(element.children[0]).to.be.ok;
+    expect(element.children[1]).to.be.ok;
+    expect(element.children[2]).not.to.be.ok;
+
+    expect(element.children[0].classList.contains('ons-tab-bar__content')).to.be.true;
+    expect(element.children[0].classList.contains('tab-bar__content')).to.be.true;
+
+    expect(element.children[1].classList.contains('tab-bar')).to.be.true;
+    expect(element.children[1].classList.contains('ons-tab-bar__footer')).to.be.true;
+    expect(element.children[1].classList.contains('ons-tabbar-inner')).to.be.true;
+  });
+})
