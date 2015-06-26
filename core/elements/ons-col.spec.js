87397bfc6ada6626b72121d3c531e942bf9668fe
anatoo <anatoo.jp@gmail.com>
2015-06-26 16:56:02 +0900

    fix(ons-col): Fixed broken assertion on ons-col's style properties.

--- core/elements/ons-col.spec.js
+++ core/elements/ons-col.spec.js
@@ -10,9 +10,15 @@ describe('ons-col', function() {
 
     expect(element.style.webkitBoxFlex).to.equal('0');
     expect(element.style.webkitFlex).to.equal('0 0 100px');
-    expect(element.style.mozBoxFlex).to.equal('0');
-    expect(element.style.mozFlex).to.equal('0 0 100px');
-    expect(element.style.msFlex).to.equal('0 0 100px');
+    if('mozBoxFlex' in element.style) {
+      expect(element.style.mozBoxFlex).to.equal('0');
+    }
+    if('mozFlex' in element.style) {
+      expect(element.style.mozFlex).to.equal('0 0 100px');
+    }
+    if('msFlex' in element.style) {
+      expect(element.style.msFlex).to.equal('0 0 100px');
+    }
     expect(element.style.flex).to.equal('0 0 100px');
     expect(element.style.maxWidth ).to.equal('100px');
   });
