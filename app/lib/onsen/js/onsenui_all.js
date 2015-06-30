b230dff18be172499304cfdf5c58c7e32e201b7f
Andreas Argelius <andreas@argeli.us>
2015-06-30 15:20:38 +0900

    chore(*): Updated generated files.

--- app/lib/onsen/js/onsenui_all.js
+++ app/lib/onsen/js/onsenui_all.js
@@ -1,4 +1,4 @@
-/*! onsenui - v1.3.2 - 2015-06-30 */
+/*! onsenui - v1.3.3 - 2015-06-30 */
 // Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 // JavaScript Dynamic Content shim for Windows Store apps
 (function () {
