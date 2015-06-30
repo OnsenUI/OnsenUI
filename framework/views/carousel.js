de8b1a76cc6a84803972933dd5c08faa3deade95
Andreas Argelius <andreas@argeli.us>
2015-06-30 12:14:05 +0900

    fix(ons-carousel): Fix bug where number of items were incorrectly cached.

--- framework/views/carousel.js
+++ framework/views/carousel.js
@@ -603,18 +603,14 @@ limitations under the License.
        * @return {Array}
        */
       _getCarouselItemElements: function() {
-        if (this._carouselItemElements && this._carouselItemElements.length) {
-          return this._carouselItemElements;
-        }
+        var nodeList = this._element[0].querySelectorAll('ons-carousel-item'),
+          rv = [];
 
-        var nodeList = this._element[0].querySelectorAll('ons-carousel-item');
-
-        this._carouselItemElements = [];
         for (var i = nodeList.length; i--; ) {
-          this._carouselItemElements.unshift(nodeList[i]);
+          rv.unshift(nodeList[i]);
         }
 
-        return this._carouselItemElements;
+        return rv;
       },
 
       /**
