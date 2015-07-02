cc13139c123882718110019c1f4cfd907c64ae8e
Andreas Argelius <andreas@argeli.us>
2015-07-02 14:02:11 +0900

    chore(*): Added test coverage for ons-core.

--- core/test/karma.conf.js
+++ core/test/karma.conf.js
@@ -12,13 +12,21 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai-as-promised', 'chai-spies', 'chai'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      __dirname + '/../../build/js/ons-core.js',
-      __dirname + '/../**/*.spec.js'
+      '../../core/vendor/winstore-jscompat.js',
+      '../../core/vendor/*.js',
+      '../../core/lib/animit.js',
+      '../../core/lib/doorlock.es6',
+      '../../core/lib/ons.es6',
+      '../../core/lib/ons-util.es6',
+      '../../core/lib/modal-animator.es6',
+      '../../core/lib/navigator-transition-animator.es6',
+      '../../core/lib/popover-animator.es6',
+      '../../core/lib/*.{es6,js}',
+      '../../core/*.{es6,js}',
+      '../../core/elements/*.{es6,js}'
     ],
-
 
     // list of files to exclude
     exclude: [
@@ -28,13 +36,38 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '../../core/**/*.es6': ['webpack'],
+      '../../core/*.es6': ['webpack']
     },
 
+    webpack: {
+        module: {
+            preLoaders: [
+                {
+                  test: /\.es6$/,
+                  loader: 'isparta'
+                }
+            ],
+        }
+    },
+
+/*    babelPreprocessor: {
+      options: {
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
+        console.log(file);
+        return file.originalPath.replace(/\.es6$/, '.js');
+      },
+      sourceFileName: function (file) {
+        return file.originalPath;
+      }
+    },*/
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
