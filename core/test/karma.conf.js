32a918ba988236f7e87b604bf4696a76f18fd1ec
Andreas Argelius <andreas@argeli.us>
2015-07-02 14:20:00 +0900

    chore(*): Add test report for CircleCI.

--- core/test/karma.conf.js
+++ core/test/karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'junit'],
 
     coverageReporter: {
       type: 'lcov',
