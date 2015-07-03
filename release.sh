da7b03df91b9c7882aa5b26455d9895c5451ace6
Andreas Argelius <andreas@argeli.us>
2015-07-03 10:37:45 +0900

    style(release.sh): Alter debug message.

--- release.sh
+++ release.sh
@@ -2,12 +2,12 @@
 
 echo "* $(tput setaf 3)Building css-components$(tput setaf 7)..."
 (cd css-components && npm install && node_modules/.bin/gulp build)
-echo "** $(tput setaf 2)Finished$(tput setaf 7)!\n"
+echo "** $(tput setaf 2)Finished$(tput setaf 7)!"
 
 echo "* $(tput setaf 3)Building OnsenUI$(tput setaf 7)..."
 npm install --loglevel error
 node_modules/.bin/gulp dist
-echo "** $(tput setaf 2)Finished$(tput setaf 7)!\n"
+echo "** $(tput setaf 2)Finished$(tput setaf 7)!"
 
 echo "* $(tput setaf 3)Copying distribution files$(tput setaf 7)"
 lasttag=`git tag -l | sort -t. -k1,1n -k2,2n -k3,3n | tail -n 1`
@@ -25,4 +25,4 @@ git commit -m "Version ${newtag} release."
 git tag -a ${newtag} -m "Version ${newtag} release."
 echo "** $(tput setaf 2)Finished$(tput setaf 7)!"
 
-echo "\n$(tput setaf 6)Complete the release with:  $(tput setaf 7)git push origin ${newtag}"
+echo "$(tput setaf 6)Complete the release with:  $(tput setaf 7)git push origin ${newtag}"
