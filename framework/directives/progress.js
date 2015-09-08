beac40b0c5c1e1fa20d32026780cd35ede1628eb
Andreas Argelius <andreas@argeli.us>
2015-09-08 17:52:36 +0900

    docs(ons-progress): Documentation for progress indicator.

---
+++ framework/directives/progress.js
@@ -0,0 +1,59 @@
+/**
+ * @ngdoc directive
+ * @id progress
+ * @name ons-progress
+ * @category progress
+ * @description
+ *   [en]A material design progress component. Can be displayed both as a linear or circular progress indicator.[/en]
+ *   [ja][/ja]
+ * @example
+ * <ons-progress
+ *  type="circular"
+ *  value="55"
+ *  secondary-value="87">
+ * </ons-progress>
+ */
+
+/**
+ * @ngdoc attribute
+ * @name type
+ * @type {String}
+ * @description
+ *   [en]The type of indicator. Can be one of either "bar" or "circular". Defaults to "bar".[/en]
+ *   [ja][/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name modifier
+ * @type {String}
+ * @description
+ *   [en]Change the appearance of the progress indicator.[/en]
+ *   [ja][/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name value
+ * @type {Number}
+ * @description
+ *   [en]Current progress. Should be a value between 0 and 100.[/en]
+ *   [ja][/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name secondary-value
+ * @type {Number}
+ * @description
+ *   [en]Current secondary progress. Should be a value between 0 and 100.[/en]
+ *   [ja][/ja]
+ */
+
+/**
+ * @ngdoc attribute
+ * @name indeterminate 
+ * @description
+ *   [en]If this attribute is set, an infinite looping animation will be shown.[/en]
+ *   [ja][/ja]
+ */
