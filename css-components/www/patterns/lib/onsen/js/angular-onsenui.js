5703889473bb8c40012d561475619cb7db1dd065
Andreas Argelius <andreas@argeli.us>
2015-10-15 16:54:14 +0900

    chore(*): Update generated files.

--- css-components/www/patterns/lib/onsen/js/angular-onsenui.js
+++ css-components/www/patterns/lib/onsen/js/angular-onsenui.js
@@ -1,4 +1,4 @@
-/*! angular-onsenui.js for onsenui - v2.0.0-alpha.1 - 2015-10-14 */
+/*! angular-onsenui.js for onsenui - v2.0.0-alpha.2 - 2015-10-15 */
 (function(module) {
 try { module = angular.module('templates-main'); }
 catch(err) { module = angular.module('templates-main', []); }
--- css-components/www/patterns/lib/onsen/js/onsenui.js
+++ css-components/www/patterns/lib/onsen/js/onsenui.js
@@ -1,4 +1,4 @@
-/*! onsenui v2.0.0-alpha.1 - 2015-10-14 */
+/*! onsenui v2.0.0-alpha.2 - 2015-10-15 */
 // Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 // JavaScript Dynamic Content shim for Windows Store apps
 (function () {
