91d2fac54c31f613668343dc44ba13d942ac0d66
Andreas Argelius <andreas@argeli.us>
2015-10-20 19:43:25 +0900

    chore(*): Update generated files.

--- app/lib/onsen/js/angular-onsenui.js
+++ app/lib/onsen/js/angular-onsenui.js
@@ -1,4 +1,4 @@
-/*! angular-onsenui.js for onsenui - v2.0.0-alpha.4 - 2015-10-20 */
+/*! angular-onsenui.js for onsenui - v2.0.0-alpha.5 - 2015-10-20 */
 (function(module) {
 try { module = angular.module('templates-main'); }
 catch(err) { module = angular.module('templates-main', []); }
