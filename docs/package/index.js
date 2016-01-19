14eaec3a2c21aa89ce0a00d2aee0cd035bca2bc1
Andreas Argelius <andreas@argeli.us>
2016-01-19 17:02:13 +0900

    fix(docs): Escape description before generating nodebb js.

--- docs/package/index.js
+++ docs/package/index.js
@@ -44,6 +44,7 @@ module.exports = new Package('ons-docs', [jsdoc, nunjucks, ngdoc])
 
     templateEngine.filters.push(require('./filters/lang'));
     templateEngine.filters.push(require('./filters/nl2ws'));
+    templateEngine.filters.push(require('./filters/escape'));
     templateEngine.filters.push(require('./filters/dump'));
 
     checkAnchorLinksProcessor.$enabled = false;
