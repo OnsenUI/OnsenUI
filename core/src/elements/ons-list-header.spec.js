2685fcf5564e07863daad5b3b153a61377b98f44
Fran Dios <frankdiox@gmail.com>
2016-02-01 20:19:21 +0900

    test(ons-list-header): Automatic Styling.

--- core/src/elements/ons-list-header.spec.js
+++ core/src/elements/ons-list-header.spec.js
@@ -35,4 +35,20 @@ describe('ons-list-header', () => {
       expect(div1.isEqualNode(div2)).to.be.true;
     });
   });
+
+  describe('autoStyling', () => {
+    it('adds \'material\' modifier on Android', () => {
+      ons.platform.select('android');
+      let e = document.createElement('ons-list-header');
+      expect(e.getAttribute('modifier')).to.equal('material');
+      ons.platform.select('');
+    });
+
+    it('removes \'material\' modifier on iOS', () => {
+      ons.platform.select('ios');
+      let e = ons._util.createElement('<ons-list-header modifier="material"></ons-list-header>');
+      expect(e.getAttribute('modifier')).not.to.equal('material');
+      ons.platform.select('');
+    });
+  });
 });
