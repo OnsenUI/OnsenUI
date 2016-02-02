1935aa4aeff2c2e1bc92e3d8dc2be6603f402234
Fran Dios <frankdiox@gmail.com>
2016-02-02 17:45:30 +0900

    refactor(ons): Auto Styling.

--- core/src/ons/ons.js
+++ core/src/ons/ons.js
@@ -133,79 +133,77 @@ ons.enableAutoStyling = () => {
  * @param {HTMLElement} element
  */
 ons._prepareAutoStyling = element => {
-  if (!ons._config.autoStyling || element.hasAttribute('disable-auto-styling')) {
-    return;
+  if (ons._config.autoStyling && !element.hasAttribute('disable-auto-styling')) {
+    let mobileOS = platform.getMobileOS();
+    if (ons._autoStyling.hasOwnProperty(mobileOS)) {
+      ons._autoStyling[mobileOS](element);
+    }
+  }
+};
+
+ons._autoStyling = {};
+
+ons._autoStyling.android = element => {
+  if (!/ons-fab|ons-speed-dial|ons-progress/.test(element.tagName.toLowerCase()) &&
+    !/material/.test(element.getAttribute('modifier'))) {
+
+    const modifiersMap = {
+      'quiet': 'material--flat',
+      'light': 'material--flat',
+      'outline': 'material--flat',
+      'cta': '',
+      'large--quiet': 'material--flat large',
+      'large--cta': 'large',
+      'noborder': '',
+      'chevron': '',
+      'tappable': ''
+    };
+
+    const oldModifier = element.getAttribute('modifier') || '';
+    element.setAttribute('modifier', '');
+
+    let newModifier = oldModifier.trim().split(/\s+/).map(e => modifiersMap.hasOwnProperty(e) ? modifiersMap[e] : e);
+    newModifier.unshift('material');
+
+    element.setAttribute('modifier', newModifier.join(' ').trim());
   }
 
-  switch (platform.getMobileOS()) {
+  if (/ons.*-button|ons-list-item|ons-fab|ons-speed-dial-item|ons-tab$/
+    .test(element.tagName.toLowerCase())
+    && !element.hasAttribute('effect')
+    && !ons._util.findChild(element, 'ons-ripple')) {
 
-    case 'android':
 
-      if (!/ons-fab|ons-speed-dial|ons-progress/.test(element.tagName.toLowerCase()) &&
-        !/material/.test(element.getAttribute('modifier'))) {
-
-        const modifiersMap = {
-          'quiet': 'material--flat',
-          'light': 'material--flat',
-          'outline': 'material--flat',
-          'cta': '',
-          'large--quiet': 'material--flat large',
-          'large--cta': 'large',
-          'noborder': '',
-          'chevron': '',
-          'tappable': ''
-        };
-
-        const oldModifier = element.getAttribute('modifier') || '';
-        element.setAttribute('modifier', '');
-
-        let newModifier = oldModifier.trim().split(/\s+/).map(e => modifiersMap.hasOwnProperty(e) ? modifiersMap[e] : e);
-        newModifier.unshift('material');
-
-        element.setAttribute('modifier', newModifier.join(' ').trim());
+    if (element.tagName.toLowerCase() === 'ons-list-item') {
+      if (element.hasAttribute('tappable')) {
+        element.setAttribute('effect', 'ripple');
+        element.removeAttribute('tappable');
       }
+    } else {
+      element.setAttribute('effect', 'ripple');
+    }
+  }
+};
 
-      if (/ons.*-button|ons-list-item|ons-fab|ons-speed-dial-item|ons-tab$/
-        .test(element.tagName.toLowerCase())
-        && !element.hasAttribute('effect')
-        && !ons._util.findChild(element, 'ons-ripple')) {
+ons._autoStyling.ios = element => {
+  if (/material/.test(element.getAttribute('modifier'))) {
+    ons._util.removeModifier(element, 'material');
 
+    if (ons._util.removeModifier(element, 'material--flat')) {
+      ons._util.addModifier(element, (ons._util.removeModifier(element, 'large')) ? 'large--quiet' : 'quiet');
+    }
 
-        if (element.tagName.toLowerCase() === 'ons-list-item') {
-          if (element.hasAttribute('tappable')) {
-            element.setAttribute('effect', 'ripple');
-            element.removeAttribute('tappable');
-          }
-        } else {
-          element.setAttribute('effect', 'ripple');
-        }
-      }
+    if (!element.getAttribute('modifier')) {
+      element.removeAttribute('modifier');
+    }
+  }
 
-      break;
+  if (element.hasAttribute('effect')) {
+    if (element.getAttribute('effect') === 'ripple' && element.tagName.toLowerCase() === 'ons-list-item') {
+      element.setAttribute('tappable', '');
+    }
 
-    case 'ios':
-
-      if (/material/.test(element.getAttribute('modifier'))) {
-        ons._util.removeModifier(element, 'material');
-
-        if (ons._util.removeModifier(element, 'material--flat')) {
-          ons._util.addModifier(element, (ons._util.removeModifier(element, 'large')) ? 'large--quiet' : 'quiet');
-        }
-
-        if (!element.getAttribute('modifier')) {
-          element.removeAttribute('modifier');
-        }
-      }
-
-      if (element.hasAttribute('effect')) {
-        if (element.getAttribute('effect') === 'ripple' && element.tagName.toLowerCase() === 'ons-list-item') {
-          element.setAttribute('tappable', '');
-        }
-
-        element.removeAttribute('effect');
-      }
-
-      break;
+    element.removeAttribute('effect');
   }
 };
 
