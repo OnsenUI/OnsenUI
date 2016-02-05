f6fcbeaa8c71e4e4d61ad012b5f61545b0f6d238
Patrick Klitzke <philologos14@gmail.com>
2016-02-05 17:50:04 +0900

    feat(ons-navigator): add "pageHTML" option to "insertPage()" method

--- core/src/elements/ons-navigator/index.js
+++ core/src/elements/ons-navigator/index.js
@@ -257,7 +257,7 @@ class NavigatorElement extends BaseElement {
     const tryInsertPage = () => {
       const unlock = this._doorLock.lock();
 
-      return internal.getPageHTMLAsync(page).then(templateHTML => {
+      var run = templateHTML => {
         const element = this._createPageElement(templateHTML);
         const pageObject = this._createPageObject(page, element, options);
 
@@ -275,7 +275,13 @@ class NavigatorElement extends BaseElement {
             }, 1000 / 60);
           });
         });
-      });
+      };
+
+      if (options.pageHTML) {
+        return run(options.pageHTML);
+      } else {
+        return internal.getPageHTMLAsync(page).then(run);
+      }
     };
 
     return new Promise(resolve => {
