b23ce3399308558a8be47c66038a3949e9282f6e
Patrick Klitzke <philologos14@gmail.com>
2016-02-19 14:52:17 +0900

    add tabbar top example

---
+++ examples/react/tabbar_top/app.jsx
@@ -0,0 +1,49 @@
+var MyPage = React.createClass({
+  render: function() {
+    return (
+      <OnsPage {...this.props}>
+        <div style={{textAlign: 'center'}}>
+          {[1,2,3,4,5, 'last'].map(function(i) { return <p>{i}</p>})}
+        </div>
+      </OnsPage>
+    );
+  },
+});
+
+var MyPage2 = React.createClass({
+  render: function() {
+    return (
+      <OnsPage {...this.props}>
+        <ons-toolbar  modifier="transparent"> <div className="center"> {this.props.title} </div></ons-toolbar> 
+      </OnsPage>
+    );
+  },
+});
+
+var MyNav  = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+
+  
+  render: function() {
+    return (
+      <OnsPage>
+      <OnsTabbar
+          position="top" var="tabbar" 
+        pages= {[
+          <MyPage />,
+          <MyPage2 title="Page 2"/>,
+          <MyPage2 title="Page 3" />
+          ]} >
+
+          <OnsTab icon="ion-home" active="true" />
+          <OnsTab icon="ion-chatbox-working"  />
+          <OnsTab icon="ion-ios-cog"  />
+        </OnsTabbar>
+      </OnsPage>
+    );
+  }
+});
+
+ReactDOM.render(<MyNav />, document.getElementById('app'));
