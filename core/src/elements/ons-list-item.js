ba3eacaab4e4eb851bf9155c2c2748a7d0f6b085
Andreas Argelius <andreas@argeli.us>
2016-02-22 11:25:02 +0900

    feat(ons-list-item, css-components): Refine styles.

--- core/src/elements/ons-list-item.js
+++ core/src/elements/ons-list-item.js
@@ -120,12 +120,10 @@ class ListItemElement extends BaseElement {
     }
 
     this.innerHTML = `
-      <label>
-        ${ripple}
-        <div class="list__item__left left">${left || ''}</div>
-        <div class="list__item__center center">${center}</div>
-        <div class="list__item__right right">${right || ''}</div>
-      </label>
+      ${ripple}
+      <div class="list__item__left left">${left || ''}</div>
+      <div class="list__item__center center">${center}</div>
+      <div class="list__item__right right">${right || ''}</div>
     `;
 
     this.classList.add('list__item');
