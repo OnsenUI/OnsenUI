4ba814623d60eca35817152e160d09400c6ba67b
Patrick Klitzke <philologos14@gmail.com>
2016-02-25 11:46:11 +0900

    add alert

---
+++ examples/react/alert/app.jsx
@@ -0,0 +1,48 @@
+var MyDialog = React.createClass({
+  okClick: function() {
+    this.refs.dialog.hide();
+  },
+  render: function() {
+    return (
+    <ons-alert-dialog ref="dialog" animation="default" cancelable>
+      <div className="alert-dialog-title">Warning!</div>
+        <div className="alert-dialog-content">
+        An error has occurred!
+        </div>
+      <div className="alert-dialog-footer">
+      <button onClick={this.okClick} className="alert-dialog-button">OK</button>
+      </div>
+    </ons-alert-dialog>
+    );
+  },
+});
+
+
+
+var MyPage  = React.createClass({
+  showAlert: function() {
+    reactUtil.createAlertDialog(
+      <MyDialog />
+    ).then(function(obj) {
+      obj.show();
+    });
+  },
+  showAlert2: function() {
+    ons.notification.alert({
+      message: 'You pressed "ok"'
+    });
+  },
+  render: function() {
+    return (
+    <OnsPage>
+      <div style={{textAlign: 'center'}}>
+        <h1>Page Content</h1>
+        <ons-button onClick={this.showAlert}> Show Alert </ons-button>
+        <ons-button onClick={this.showAlert2}> Show Alert 2 </ons-button>
+      </div>
+    </OnsPage>
+    );
+  }
+});
+
+ReactDOM.render(<MyPage />, document.getElementById('app'));
