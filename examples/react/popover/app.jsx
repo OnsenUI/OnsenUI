13176646c99a72d4d95c05ab27b7a0fd9f196493
Patrick Klitzke <philologos14@gmail.com>
2016-02-26 12:29:55 +0900

    update popover example

--- examples/react/popover/app.jsx
+++ examples/react/popover/app.jsx
@@ -30,21 +30,21 @@ var MyPage  = React.createClass({
   render: function() {
     return (
       <OnsPage>
-      {/* <div className="navigation-bar"> */}
-      {/*   <div className="navigation-bar__left"> */}
-      {/*     <span ref="navigation"  onClick={this.showClick(this.refs.navigation)} className="toolbar-button--outline navigation-bar__line-height"> */}
-      {/*       <i className="ion-navicon" style={{fontSize: 32, verticalAlign: -6}}></i> */}
-      {/*     </span> */}
-      {/*   </div> */}
-      {/*  */}
-      {/*   <div className="navigation-bar__center"> */}
-      {/*     Popover demo! */}
-      {/*   </div> */}
-      {/*  */}
-      {/*   <div className="navigation-bar__right"> */}
-      {/*     <span ref="topRight" onClick={this.showClick(this.refs.topRight)} className="toolbar-button--outline navigation-bar__line-height">Button</span> */}
-      {/*   </div> */}
-      {/* </div> */}
+      <div className="navigation-bar">
+        <div className="navigation-bar__left">
+          <span ref="navigation" onClick={() => this.showClick(this.refs.navigation)} className="toolbar-button--outline navigation-bar__line-height">
+            <i className="ion-navicon" style={{fontSize: 32, verticalAlign: -6}}></i>
+          </span>
+        </div>
+
+        <div className="navigation-bar__center">
+          Popover demo!
+        </div>
+
+        <div className="navigation-bar__right">
+          <span ref="topRight" onClick={() => this.showClick(this.refs.topRight)} className="toolbar-button--outline navigation-bar__line-height">Button</span>
+        </div>
+      </div>
 
       <div style={{textAlign: 'center'}}>
         <br />
@@ -52,42 +52,42 @@ var MyPage  = React.createClass({
         <ons-button ref="button" onClick={() => this.showClick(this.refs.button)}>Click me!</ons-button>
       </div>
 
-      {/* <div className="tab-bar"> */}
-      {/*   <label onClick={this.show.bind(this, '#stop')} className="tab-bar__item"> */}
-      {/*     <input type="radio" name="tab-bar-b" defaultChecked="checked" /> */}
-      {/*     <button id="stop" className="tab-bar__button"> */}
-      {/*       <i className="tab-bar__icon ion-stop"></i> */}
-      {/*     </button> */}
-      {/*   </label> */}
-      {/*  */}
-      {/*   <label onClick={this.show.bind(this, '#record')} className="tab-bar__item"> */}
-      {/*     <input type="radio" name="tab-bar-b" /> */}
-      {/*     <button id="record" className="tab-bar__button"> */}
-      {/*       <i className="tab-bar__icon ion-record"></i> */}
-      {/*     </button> */}
-      {/*   </label> */}
-      {/*  */}
-      {/*   <label onClick={this.show.bind(this, '#star')} className="tab-bar__item"> */}
-      {/*     <input type="radio" name="tab-bar-b" /> */}
-      {/*     <button id="star" className="tab-bar__button"> */}
-      {/*       <i className="tab-bar__icon ion-star"></i> */}
-      {/*     </button> */}
-      {/*   </label> */}
-      {/*  */}
-      {/*   <label onClick={this.show.bind(this, '#cloud')} className="tab-bar__item"> */}
-      {/*     <input type="radio" name="tab-bar-b" /> */}
-      {/*     <button id="cloud" className="tab-bar__button"> */}
-      {/*       <i className="tab-bar__icon ion-ios-cloud-outline"></i> */}
-      {/*     </button> */}
-      {/*   </label> */}
-      {/*  */}
-      {/*   <label onClick={() => this.setState({target: this.refs.pie})} className="tab-bar__item"> */}
-      {/*     <input type="radio" name="tab-bar-b" /> */}
-      {/*     <button ref="pie" className="tab-bar__button"> */}
-      {/*       <i className="tab-bar__icon ion-ios-pie"></i> */}
-      {/*     </button> */}
-      {/*   </label> */}
-      {/* </div> */}
+      <div className="tab-bar">
+        <label onClick={() => this.showClick(this.refs.stop)} className="tab-bar__item">
+          <input type="radio" name="tab-bar-b" defaultChecked="checked" />
+          <button ref="stop" className="tab-bar__button">
+            <i className="tab-bar__icon ion-stop"></i>
+          </button>
+        </label>
+
+        <label onClick={() => this.showClick(this.refs.record)} className="tab-bar__item">
+          <input type="radio" name="tab-bar-b" />
+          <button ref="record" className="tab-bar__button">
+            <i className="tab-bar__icon ion-record"></i>
+          </button>
+        </label>
+
+        <label onClick={() => this.showClick(this.refs.star)}  className="tab-bar__item">
+          <input type="radio" name="tab-bar-b" />
+          <button ref="star" className="tab-bar__button">
+            <i className="tab-bar__icon ion-star"></i>
+          </button>
+        </label>
+
+        <label onClick={() => this.showClick(this.refs.cloud)} className="tab-bar__item">
+          <input type="radio" name="tab-bar-b" />
+          <button ref="cloud" className="tab-bar__button">
+            <i className="tab-bar__icon ion-ios-cloud-outline"></i>
+          </button>
+        </label>
+
+        <label onClick={() => this.showClick(this.refs.pie)} className="tab-bar__item">
+          <input type="radio" name="tab-bar-b" />
+          <button ref="pie" className="tab-bar__button">
+            <i className="tab-bar__icon ion-ios-pie"></i>
+          </button>
+        </label>
+      </div>
       <OnsPopover isOpen={this.state.isOpen} onCancel={this.cancel} getTarget={this.getTarget} direction="up down" cancelable>
         <div style={{textAlign: 'center', opacity: 0.5}}>
           <p>This is a popover!</p>
