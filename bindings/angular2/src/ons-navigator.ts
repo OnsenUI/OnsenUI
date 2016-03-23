466a65285dba6cede7b2bd68d974ad13fff545d1
Mitsunori Kubota <anatoo.jp@gmail.com>
2016-03-23 20:09:51 +0900

    docs(angular2-binding): Added some doc comments.

--- bindings/angular2/src/ons-navigator.ts
+++ bindings/angular2/src/ons-navigator.ts
@@ -35,6 +35,9 @@ export class NavigatorPage {
   }
 }
 
+/**
+ * @element ons-navigator
+ */
 @Directive({
   selector: 'ons-navigator > ons-page'
 })
@@ -55,18 +58,33 @@ export class OnsNavigator {
    * @method pushComponent
    * @signature pushComponent(type: Type, params: Map = {})
    * @param {Type} type
-   * @param {Map} params
+   *   [en][/en]
+   *   [ja]navigatorに挿入するコンポーネントのクラスを指定します。[/ja]
+   * @param {Map} [params]
+   *   [en][/en]
+   *   [ja]
+   *     新しく生成するページへのパラメータを指定します。
+   *     navigatorに挿入されたコンポーネントが初期化される際に、
+   *     このパラメータをコンストラクタから受け取ることができます。
+   *   [/ja]
+   * @return {Promise}
+   *   [en][/en]
+   *   [ja]コンポーネントが生成され、遷移アニメーションが終わった時に解決されます。[/ja]
+   * @description
+   *   [en][/en]
+   *   [ja][/ja]
    */
   pushComponent(type: Type, params: Object = {}): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.loadPageComponent(type, params, navigatorPage => {
+      this._loadPageComponent(type, params, navigatorPage => {
+        // TODO implement animation
         this._pages.push(navigatorPage);
         resolve(navigatorPage);
       });
     });
   }
 
-  private loadPageComponent(type: Type, params: Object, done: Function): void {
+  private _loadPageComponent(type: Type, params: Object, done: Function): void {
     this._compiler.compileInHost(type).then(hostProtoViewRef => {
       const location = this._elementRef;
       const pageParams = new PageParams(params);
@@ -91,11 +109,26 @@ export class OnsNavigator {
 
   popComponent(): Promise<any> {
     return new Promise((resolve, reject) => {
+      // TODO implement animation
       const page: NavigatorPage = this._pages.pop();
       page.dispose();
 
       resolve();
     });
+  }
+
+  insertComponent(type: Type, index: number): Promise<any> {
+    // TODO implement
+    return Promise.resolve();
+  }
+
+  destoryComponent(index: number): Promise<any> {
+    // TODO implement
+    return Promise.resolve();
+  }
+
+  private update(): void {
+    // TODO implement
   }
 }
 
