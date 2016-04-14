312575650bef5fd9c655b0f46926f1e9488561c3
Mitsunori Kubota <anatoo.jp@gmail.com>
2016-04-14 18:20:49 +0900

    fix(angular2-binding): Trivial fix.

--- bindings/angular2/examples/tabbar.ts
+++ bindings/angular2/examples/tabbar.ts
@@ -14,7 +14,7 @@ import {
       <div class="center">Page</div>
     </ons-toolbar>
     <div class="page__background"></div>
-    <div class="page__content" no-status-bar-fill>
+    <div class="page__content">
       <div style="text-align: center; margin: 10px">
         <ons-button (click)="push()">push</ons-button>
         <ons-button (click)="pop()">pop</ons-button>
@@ -33,9 +33,9 @@ export class PageComponent {
   directives: [OnsTabbar, OnsTab],
   template: `
   <ons-tabbar>
-    <ons-tab (page)="PageComponent" label="Page1" icon="ion-home"></ons-tab>
-    <ons-tab (page)="PageComponent" label="Page2" icon="ion-help"></ons-tab>
-    <ons-tab (page)="PageComponent" label="Page3" icon="ion-stop"></ons-tab>
+    <ons-tab label="Page1" icon="ion-home"></ons-tab>
+    <ons-tab label="Page2" icon="ion-help"></ons-tab>
+    <ons-tab label="Page3" icon="ion-stop"></ons-tab>
   </ons-tabbar>
   `
 })
