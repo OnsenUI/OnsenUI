277245e87f04ee70312a88ff9183947cff695cee
Andreas Argelius <andreas@argeli.us>
2016-04-21 18:43:50 +0900

    docs(ons-splitter-side): update docs

--- core/src/elements/ons-splitter/index.js
+++ core/src/elements/ons-splitter/index.js
@@ -123,7 +123,7 @@ class SplitterElement extends BaseElement {
   }
 
   _onDeviceBackButton(handler) {
-    this._sides.some(s => s.isOpen() ? s.close() : false) || handler.callParentHandler();
+    this._sides.some(s => s.isOpen ? s.close() : false) || handler.callParentHandler();
   }
 
   _onModeChange(e) {
