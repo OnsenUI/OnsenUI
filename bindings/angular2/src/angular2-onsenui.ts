bd0cc13f0b1df19af72148209cf09e79e60f7241
Mitsunori Kubota <anatoo.jp@gmail.com>
2016-04-22 18:08:33 +0900

    feat(angular2-binding): Added a directive and an example for "ons-input" element.

--- bindings/angular2/src/angular2-onsenui.ts
+++ bindings/angular2/src/angular2-onsenui.ts
@@ -10,6 +10,9 @@ export * from './directives/ons-alert-dialog';
 export * from './directives/ons-popover';
 export * from './directives/ons-switch';
 export * from './directives/ons-range';
+export * from './directives/ons-input';
+
+
 export * from './ons/notification';
 export * from './ons/platform';
 export * from './ons/createAlertDialog';
@@ -18,3 +21,23 @@ export * from './ons/createPopover';
 export function bootstrap(type: Type, providers: Array<any> = []) {
   return browser.bootstrap(type, providers);
 };
+
+import {OnsNavigator} from './directives/ons-navigator';
+import {OnsCarousel} from './directives/ons-carousel';
+import {OnsTabbar} from './directives/ons-tabbar';
+import {OnsAlertDialog} from './directives/ons-alert-dialog';
+import {OnsPopover} from './directives/ons-popover';
+import {OnsSwitch} from './directives/ons-switch';
+import {OnsRange} from './directives/ons-range';
+import {OnsInput} from './directives/ons-input';
+
+export const ONS_DIRECTIVES = [
+  OnsNavigator,
+  OnsCarousel,
+  OnsTabbar,
+  OnsAlertDialog,
+  OnsPopover,
+  OnsSwitch,
+  OnsRange,
+  OnsInput
+];
