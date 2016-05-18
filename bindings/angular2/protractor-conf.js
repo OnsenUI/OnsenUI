25059acdb7af84a137c0d833be9979e15646ae36
Mitsunori Kubota <anatoo.jp@gmail.com>
2016-05-18 21:06:39 +0900

    chore(angular2-binding): Made spec patterns to absolute path again.

--- bindings/angular2/protractor-conf.js
+++ bindings/angular2/protractor-conf.js
@@ -2,8 +2,8 @@ require('babel-register');
 
 exports.config = {
   seleniumServerJar: require('selenium-server-standalone-jar').path,
-  chromeDriver: __dirname + '/node_modules/chromedriver/bin/chromedriver',
-  specs: ['examples/*.spec.js'],
+  chromeDriver: 'node_modules/chromedriver/bin/chromedriver',
+  specs: [__dirname + '/examples/*.spec.js'],
   directConnect: true,
   baseUrl: 'http://localhost:9090/',
   useAllAngular2AppRoots: true,
