34f0202330ceb20b9e6b520f9fe1de4e6b395e0d
anatoo <anatoo.jp@gmail.com>
2016-06-23 19:33:58 +0900

    fix(angular2-binding): Removed unneeded imports.

--- bindings/angular2/src/vendor.ts
+++ bindings/angular2/src/vendor.ts
@@ -1,11 +1,5 @@
 // Vendors
 
-// Angular 2
-import '@angular/platform-browser-dynamic';
-import '@angular/core';
-import '@angular/http';
-import '@angular/router';
-
 // RxJS 5
 import 'rxjs/Rx';
 
