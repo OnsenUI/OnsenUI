9b0530e2979489c5b8b20dc40cf28463df10870d
anatoo <anatoo.jp@gmail.com>
2016-10-06 19:33:18 +0900

    chore(angular1-binding): Updated generated templates.

--- bindings/angular1/directives/templates.js
+++ bindings/angular1/directives/templates.js
@@ -5,12 +5,12 @@ catch(err) { app = angular.module("templates-main", []); }
 app.run(["$templateCache", function($templateCache) {
 "use strict";
 
-$templateCache.put("templates/sliding_menu.tpl","<div class=\"onsen-sliding-menu__menu ons-sliding-menu-inner\"></div>\n" +
-    "<div class=\"onsen-sliding-menu__main ons-sliding-menu-inner\"></div>\n" +
+$templateCache.put("templates/sliding_menu.tpl","<div class=\"onsen-sliding-menu__menu\"></div>\n" +
+    "<div class=\"onsen-sliding-menu__main\"></div>\n" +
     "")
 
-$templateCache.put("templates/split_view.tpl","<div class=\"onsen-split-view__secondary full-screen ons-split-view-inner\"></div>\n" +
-    "<div class=\"onsen-split-view__main full-screen ons-split-view-inner\"></div>\n" +
+$templateCache.put("templates/split_view.tpl","<div class=\"onsen-split-view__secondary full-screen\"></div>\n" +
+    "<div class=\"onsen-split-view__main full-screen\"></div>\n" +
     "")
 }]);
 })();
