e8c87494cc1c0f15c947d08008c7e66c876d737a
Andreas Argelius <andreas@argeli.us>
2016-11-01 15:53:26 +0900

    test(angular2): add tests for carousel

--- bindings/angular2/examples/carousel.ts
+++ bindings/angular2/examples/carousel.ts
@@ -12,25 +12,25 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
   template: `
   <ons-page>
     <ons-toolbar>
-      <div class="left"><ons-toolbar-button (click)="myCarousel.prev()">Prev</ons-toolbar-button></div>
+      <div class="left"><ons-toolbar-button id="prev" (click)="myCarousel.prev()">Prev</ons-toolbar-button></div>
       <div class="center">Carousel Example</div>
-      <div class="right"><ons-toolbar-button (click)="myCarousel.next()">Next</ons-toolbar-button></div>
+      <div class="right"><ons-toolbar-button id="next" (click)="myCarousel.next()">Next</ons-toolbar-button></div>
     </ons-toolbar>
 
     <div class="background"></div>
     <div class="content">
 
       <ons-carousel #myCarousel swipeable overscrollable auto-scroll fullscreen auto-scroll-ratio="0.4" animation-options="{duration: 0.1}">
-        <ons-carousel-item style="background-color: #009">
+        <ons-carousel-item id="slide-1" style="background-color: #009">
           <div style="text-align: center; font-size: 88px; color: white; margin-top: 100px;">1</div>
         </ons-carousel-item>
-        <ons-carousel-item style="background-color: gray">
+        <ons-carousel-item id="slide-2" style="background-color: gray">
           <div style="text-align: center; font-size: 88px; color: white; margin-top: 100px;">2</div>
         </ons-carousel-item>
-        <ons-carousel-item style="background-color: red">
+        <ons-carousel-item id="slide-3" style="background-color: red">
           <div style="text-align: center; font-size: 88px; color: white; margin-top: 100px;">3</div>
         </ons-carousel-item>
-        <ons-carousel-item style="background-color: green">
+        <ons-carousel-item id="slide-4" style="background-color: green">
           <div style="text-align: center; font-size: 88px; color: white; margin-top: 100px;">4</div>
         </ons-carousel-item>
       </ons-carousel>
