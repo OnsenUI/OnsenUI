d4cb8775a189ddfb7c2c441ed1db613b13a22ca4
Andreas Argelius <andreas@argeli.us>
2016-11-09 16:19:39 +0900

    test(angular2): add tests for range

--- bindings/angular2/examples/range.spec.js
+++ bindings/angular2/examples/range.spec.js
@@ -1,3 +1,5 @@
+const EC = protractor.ExpectedConditions;
+
 describe('range.html', () => {
   beforeEach(() => {
     browser.get('/bindings/angular2/examples/range.html');
@@ -5,5 +7,18 @@ describe('range.html', () => {
 
   it('should have ons-range elements', () => {
     expect($('ons-range').isPresent()).toBeTruthy();
+  });
+
+  it('should change the value when clicked', () => {
+    expect($('#value').getText()).toBe('10');
+
+    browser.actions()
+      .mouseMove($('#range'), {x: 16, y: 16})
+      .mouseDown()
+      .mouseMove({x: 50, y: 16})
+      .mouseUp()
+      .perform();
+
+    expect($('#value').getText()).not.toBe('10');
   });
 });
