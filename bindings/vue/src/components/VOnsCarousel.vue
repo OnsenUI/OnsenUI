b610f19138e10ac01def4e07f6367ccad0fe1197
Fran Dios <frankdiox@gmail.com>
2017-01-16 10:04:59 +0100

    refactor(vue): Carousel uses mixins.

--- bindings/vue/src/components/VOnsCarousel.vue
+++ bindings/vue/src/components/VOnsCarousel.vue
@@ -5,66 +5,9 @@
 </template>
 
 <script>
-  import ons from 'onsenui';
-
-  import {createComputedPropertiesFor, createMethodsFor} from '../internal/optionsObjectHelper.js';
+  import { deriveEvents, deriveMethods, deriveProperties } from '../internal/mixins.js';
 
   export default {
-    computed: createComputedPropertiesFor(ons.CarouselElement),
-
-    methods: Object.assign(
-      createMethodsFor(ons.CarouselElement),
-      {
-        onPostChange(ev) {
-          this.$emit('postchange', ev);
-        },
-        onRefresh(ev) {
-          this.$emit('refresh', ev);
-        },
-        onOverScroll(ev) {
-          this.$emit('overscroll', ev);
-        }
-      }
-    ),
-
-    //--------------------------------
-    // lifecycle hooks
-    //--------------------------------
-
-    // beforeCreate() {
-    // },
-
-    // created() {
-    // },
-
-    // beforeMount() {
-    // },
-
-    mounted() {
-      this.$el.addEventListener('postchange', this.onPostChange);
-      this.$el.addEventListener('refresh', this.onRefresh);
-      this.$el.addEventListener('overscroll', this.onOverScroll);
-    },
-
-    // beforeUpdate() {
-    // },
-
-    // updated() {
-    // },
-
-    // activated() {
-    // },
-
-    // deactivated() {
-    // },
-
-    beforeDestroy() {
-      this.$el.removeEventListener('postchange', this.onPostChange);
-      this.$el.removeEventListener('refresh', this.onRefresh);
-      this.$el.removeEventListener('overscroll', this.onOverScroll);
-    },
-
-    // destroyed() {
-    // },
+    mixins: [deriveEvents, deriveMethods, deriveProperties]
   };
 </script>
