a4a7a65ec4fa2d7a24281c15062baf629272de84
Fran Dios <frankdiox@gmail.com>
2017-01-16 10:04:42 +0100

    feat(vue): Add Carousel example.

---
+++ bindings/vue/examples/components/Carousel.vue
@@ -0,0 +1,29 @@
+<template>
+  <ons-page>
+    <v-ons-carousel fullscreen swipeable auto-scroll overscrollable @postchange="log('postchange!!')" @refresh="log('refresh!!')" @overscroll="log('overscroll!!')">
+      <v-ons-carousel-item v-for="(value, key) in items" :style="{backgroundColor: value}">
+        <div style="text-align: center; font-size: 30px; margin-top: 20px; color: #fff;">{{key}}</div>
+      </v-ons-carousel-item>
+    </v-ons-carousel>
+  </ons-page>
+</template>
+
+<script>
+	export default {
+    data: function() {
+      return {
+        items: {
+          BLUE: '#085078',
+          DARK: '#373B44',
+          ORANGE: '#D38312'
+        }
+      }
+    },
+
+    methods: {
+      log: function(msg) {
+        console.log(msg);
+      }
+    }
+	};
+</script>
--- bindings/vue/examples/components/index.js
+++ bindings/vue/examples/components/index.js
@@ -1,19 +1,21 @@
 import Tabbar from './Tabbar.vue';
+import Navigator from './Navigator.vue';
 import PullHook from './PullHook.vue';
 import Splitter from './Splitter.vue';
 import Fab from './Fab.vue';
 import SpeedDial from './SpeedDial.vue';
 import Dialogs from './Dialogs.vue';
+import Carousel from './Carousel.vue';
 import Forms from './Forms.vue';
-import Navigator from './Navigator.vue';
 
 export {
   Tabbar,
+  Navigator,
   PullHook,
   Splitter,
   Fab,
   SpeedDial,
   Dialogs,
-  Forms,
-  Navigator
+  Carousel,
+  Forms
 };
