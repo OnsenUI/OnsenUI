46e3104bfd3b4640e4a0b043e599d6fd6da4c2fc
anatoo <anatoo.jp@gmail.com>
2017-01-18 13:26:49 +0900

    Revert "fix(angular2-binding): Fixed script loading order for all examples."
    This reverts commit 2334be8d6d3e370da5fd95383e8651298f7333e4.

--- bindings/angular2/examples/splitter.ts
+++ bindings/angular2/examples/splitter.ts
@@ -28,56 +28,38 @@ class ContentPageComponent {
     </ons-toolbar>
     <div class="background"></div>
     <div class="content">
-      {{msg}}
+      Left
     </div>
   `
 })
-class LeftPageComponent {
-  msg = 'Left'
-}
-
-@Component({
-  selector: 'ons-page',
-  template: `
-    <ons-toolbar>
-      <div class="center">Right Page</div> 
-    </ons-toolbar>
-    <div class="background"></div>
-    <div class="content">
-      {{msg}}
-    </div>
-  `
-})
-class RightPageComponent {
-  msg = 'Right'
+class SidePageComponent {
 }
 
 @Component({
   selector: 'app',
   template: `
   <ons-splitter>
-    <ons-splitter-side [page]="leftPage" side="left" width="200px" style="border-right: 1px solid #ccc">
+    <ons-splitter-side [page]="sidePage" side="left" width="200px" style="border-right: 1px solid #ccc">
     </ons-splitter-side>
 
     <ons-splitter-content [page]="contentPage">
     </ons-splitter-content>
 
-    <ons-splitter-side [page]="rightPage" side="right" collapse swipeable width="200px" style="border-left: 1px solid #ccc">
+    <ons-splitter-side [page]="sidePage" side="right" collapse swipeable width="200px" style="border-right: 1px solid #ccc">
     </ons-splitter-side>
   </ons-splitter>
   `
 })
 export class AppComponent {
-  leftPage = LeftPageComponent;
-  rightPage = RightPageComponent;
+  sidePage = SidePageComponent;
   contentPage = ContentPageComponent;
 }
 
 @NgModule({
   imports: [OnsenModule],
-  declarations: [AppComponent, LeftPageComponent, RightPageComponent, ContentPageComponent],
+  declarations: [AppComponent, SidePageComponent, ContentPageComponent],
   bootstrap: [AppComponent],
-  entryComponents: [LeftPageComponent, RightPageComponent, ContentPageComponent],
+  entryComponents: [SidePageComponent, ContentPageComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 class AppModule { }
