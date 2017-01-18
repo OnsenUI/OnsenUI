ca9717f238eced77b2b5f5de97c7aac5ff559e76
anatoo <anatoo.jp@gmail.com>
2017-01-18 13:12:13 +0900

    fix(angular2-binding): Changed to use NgZone manually on creating components dynamically.

--- bindings/angular2/src/ons/alert-dialog-factory.ts
+++ bindings/angular2/src/ons/alert-dialog-factory.ts
@@ -5,7 +5,8 @@ import {
   ApplicationRef,
   ComponentRef,
   ReflectiveInjector,
-  Type
+  Type,
+  NgZone
 } from '@angular/core';
 import {Params} from './params';
 import {ComponentLoader} from './component-loader';
@@ -25,30 +26,33 @@ export class AlertDialogFactory {
     private _injector: Injector,
     private _resolver: ComponentFactoryResolver,
     private _appRef: ApplicationRef,
-    private _componentLoader: ComponentLoader
+    private _componentLoader: ComponentLoader,
+    private _zone: NgZone
   ) {
   }
 
   createAlertDialog(componentType: Type<any>, params: Object = {}): Promise<AlertDialogRef> {
     return new Promise(resolve => {
       setImmediate(() => {
-        const factory = this._resolver.resolveComponentFactory(componentType);
-        const injector = ReflectiveInjector.resolveAndCreate([
-          {provide: Params, useValue: new Params(params)}
-        ], this._injector);
-        const componentRef = factory.create(injector);
-        const rootElement = componentRef.location.nativeElement;
+        this._zone.run(() => {
+          const factory = this._resolver.resolveComponentFactory(componentType);
+          const injector = ReflectiveInjector.resolveAndCreate([
+            {provide: Params, useValue: new Params(params)}
+          ], this._injector);
+          const componentRef = factory.create(injector);
+          const rootElement = componentRef.location.nativeElement;
 
-        this._componentLoader.load(componentRef);
+          this._componentLoader.load(componentRef);
 
-        const element = rootElement.children[0];
-        const alertDialogElement = element.tagName === 'ONS-ALERT-DIALOG' ? element : element.querySelector('ons-alert-dialog');
+          const element = rootElement.children[0];
+          const alertDialogElement = element.tagName === 'ONS-ALERT-DIALOG' ? element : element.querySelector('ons-alert-dialog');
 
-        if (!alertDialogElement) {
-          throw Error('<ons-alert-dialog> element is not found in component\'s template.');
-        }
+          if (!alertDialogElement) {
+            throw Error('<ons-alert-dialog> element is not found in component\'s template.');
+          }
 
-        resolve({alertDialog: alertDialogElement, destroy: () => componentRef.destroy()});
+          resolve({alertDialog: alertDialogElement, destroy: () => componentRef.destroy()});
+        });
       });
     });
   }
