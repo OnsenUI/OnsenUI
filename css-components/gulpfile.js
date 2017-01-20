e977a3ca389f2946c3b77d40aa55ce013ab038b4
anatoo <anatoo.jp@gmail.com>
2017-01-20 18:29:45 +0900

    feat(css-components): Added css-compoonents previewer.

--- css-components/gulpfile.js
+++ css-components/gulpfile.js
@@ -5,6 +5,9 @@ const gutil = require('gulp-util');
 const runSequence = require('run-sequence');
 const browserSync = require('browser-sync');
 const $ = require('gulp-load-plugins')();
+const eco = require('eco');
+const fs = require('fs');
+const ancss = require('ancss');
 
 ////////////////////////////////////////
 // build
@@ -18,9 +21,27 @@ gulp.task('build', () => {
 });
 
 ////////////////////////////////////////
+// generate-preview
+////////////////////////////////////////
+gulp.task('generate-preview', ['build'], () => {
+  const template = fs.readFileSync(__dirname + '/templates/preview.html.eco', 'utf-8');
+  const css = fs.readFileSync(__dirname + '/build/onsen-css-components.css', 'utf-8');
+  const components = ancss.parse(css, {detect: line => line.match(/topdoc/)});
+  fs.writeFileSync(__dirname + '/build/preview.html', eco.render(template, {components}), 'utf-8');
+});
+
+////////////////////////////////////////
 // serve
 ////////////////////////////////////////
-gulp.task('serve', ['build'], done => {
-  gulp.watch(['src/**/*.less'], ['build']);
+gulp.task('serve', ['generate-preview'], done => {
+  gulp.watch(['src/**/*.less', 'templates/preview.html.eco'], ['generate-preview']);
+  gulp.watch('build/preview.html').on('change', browserSync.reload);
+
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    },
+    startPath: '/preview.html'
+  });
 });
 
