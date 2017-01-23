cd605a30a6170f150bbbe6a05350bd8b7add3d8b
Fran Dios <frankdiox@gmail.com>
2017-01-23 12:47:22 +0900

    fix(ons-popover): zIndex is not necessary.

--- core/src/elements/ons-popover/index.js
+++ core/src/elements/ons-popover/index.js
@@ -326,7 +326,6 @@ export default class PopoverElement extends BaseElement {
     }
 
     this.classList.add(defaultClassName);
-    this.style.zIndex = 10001;
 
     const hasDefaultContainer = this._popover && this._content;
 
