ddae83c91b49681bc4ed3355a9b1556a2db8790a
Naoki Matagawa <naoki.m@asial.co.jp>
2017-02-01 19:44:06 +0900

    fix(vue): Set `vue` as an external module.

--- bindings/vue/webpack.config.prod.js
+++ bindings/vue/webpack.config.prod.js
@@ -21,6 +21,14 @@ module.exports = {
         commonjs2: 'onsenui',
         amd: 'onsenui'
       }
+    },
+    {
+      vue: {
+        root: 'Vue',
+        commonjs: 'vue',
+        commonjs2: 'vue',
+        amd: 'vue'
+      }
     }
   ],
   resolveLoader: {
