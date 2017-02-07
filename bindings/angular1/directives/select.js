a1f566e69056397d8229c36e179eb211a7861794
misterjunio <ruca.33.maia@gmail.com>
2017-02-07 12:16:15 +0900

    docs(angular1): Added 'ons-select' wrapper docs.

--- bindings/angular1/directives/select.js
+++ bindings/angular1/directives/select.js
@@ -1,6 +1,47 @@
-
 /**
  * @element ons-select
+ */
+
+/**
+ * @method on
+ * @signature on(eventName, listener)
+ * @description
+ *   [en]Add an event listener.[/en]
+ *   [ja]イベントリスナーを追加します。[/ja]
+ * @param {String} eventName
+ *   [en]Name of the event.[/en]
+ *   [ja]イベント名を指定します。[/ja]
+ * @param {Function} listener
+ *   [en]Function to execute when the event is triggered.[/en]
+ *   [ja]このイベントが発火された際に呼び出される関数オブジェクトを指定します。[/ja]
+ */
+
+/**
+ * @method once
+ * @signature once(eventName, listener)
+ * @description
+ *  [en]Add an event listener that's only triggered once.[/en]
+ *  [ja]一度だけ呼び出されるイベントリスナーを追加します。[/ja]
+ * @param {String} eventName
+ *   [en]Name of the event.[/en]
+ *   [ja]イベント名を指定します。[/ja]
+ * @param {Function} listener
+ *   [en]Function to execute when the event is triggered.[/en]
+ *   [ja]イベントが発火した際に呼び出される関数オブジェクトを指定します。[/ja]
+ */
+
+/**
+ * @method off
+ * @signature off(eventName, [listener])
+ * @description
+ *  [en]Remove an event listener. If the listener is not specified all listeners for the event type will be removed.[/en]
+ *  [ja]イベントリスナーを削除します。もしイベントリスナーを指定しなかった場合には、そのイベントに紐づく全てのイベントリスナーが削除されます。[/ja]
+ * @param {String} eventName
+ *   [en]Name of the event.[/en]
+ *   [ja]イベント名を指定します。[/ja]
+ * @param {Function} listener
+ *   [en]Function to execute when the event is triggered.[/en]
+ *   [ja]削除するイベントリスナーを指定します。[/ja]
  */
 
 (function () {
