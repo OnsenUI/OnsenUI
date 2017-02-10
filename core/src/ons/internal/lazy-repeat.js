6ead88b65d018c5ce6dbe350a6d8ace2b6de8acb
Fran Dios <frankdiox@gmail.com>
2017-02-10 16:46:58 +0900

    fix(ons-lazy-repeat): Supports Safari scroll.

--- core/src/ons/internal/lazy-repeat.js
+++ core/src/ons/internal/lazy-repeat.js
@@ -391,10 +391,9 @@ export class LazyRepeatProvider {
   _calculateStartIndex(current) {
     const firstItemIndex = this._firstItemRendered();
     const lastItemIndex = this._lastItemRendered();
-    if (this._topPositions[lastItemIndex] - this._topPositions[firstItemIndex] === 0) {
-      // Fix for Angular 2 - only needed for the first render
-      this._recalculateTopPositions(firstItemIndex, lastItemIndex);
-    }
+
+    // Fix for Safari scroll and Angular 2
+    this._recalculateTopPositions(firstItemIndex, lastItemIndex);
 
     let start = 0;
     let end = this._countItems() - 1;
