cf1f409ccd29901b02c61cd8ca2a89f9ee91c99c
Fran Dios <frankdiox@gmail.com>
2017-02-24 13:34:05 +0900

    refactor(vue): Splitter API.

---
+++ bindings/vue/src/components/VOnsSplitter.vue
@@ -0,0 +1,14 @@
+<template>
+  <ons-splitter>
+    <slot></slot>
+    <v-ons-splitter-mask :onClick="() => this.$emit('mask')"></v-ons-splitter-mask>
+  </ons-splitter>
+</template>
+
+<script>
+  import { deriveProperties } from '../mixins';
+
+  export default {
+    mixins: [deriveProperties]
+  };
+</script>
---
+++ bindings/vue/src/components/VOnsSplitterSide.vue
@@ -0,0 +1,49 @@
+<template>
+  <ons-splitter-side :swipeable="!!swipeable">
+    <slot></slot>
+  </ons-splitter-side>
+</template>
+
+<script>
+  import { destroyable, hasOptions, VuePageLoader, deriveEvents, deriveMethods, deriveProperties } from '../mixins';
+
+  export default {
+    mixins: [VuePageLoader, deriveEvents, deriveProperties],
+
+    props: {
+      open: {
+        type: Boolean,
+        required: true
+      },
+
+      swipeable: {
+        type: Function
+      }
+    },
+
+    watch: {
+      open() {
+        this.$el[this.open ? 'open' : 'close'].call(this.$el);
+      }
+    },
+
+    mounted() {
+      this.$on('swipeEnd', shouldOpen => {
+        if (this.open !== shouldOpen) {
+          return this.swipeable(shouldOpen);
+        }
+        if (this.open !== this.$el.isOpen) {
+          this.$el[this.open ? 'open' : 'close'].call(this.$el);
+        }
+      });
+
+      this.splitter.$on('mask', () => this.swipeable && this.swipeable(false));
+
+      this.$nextTick(() => {
+        if (this.open !== this.$el.isOpen) {
+          this.$el[this.open ? 'open' : 'close'].call(this.$el);
+        }
+      });
+    }
+  };
+</script>
