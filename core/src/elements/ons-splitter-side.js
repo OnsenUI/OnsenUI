6daaeffaf45a88cc52b598e7ab5b38e5842975ec
Fran Dios <frankdiox@gmail.com>
2017-02-28 16:02:41 +0900

    Revert "refactor(ons-splitter-side): Provides onSwipeEnd hook."
    This reverts commit 6200f1c8ef221c2f7594ea406c9f33de66d5b31b.

--- core/src/elements/ons-splitter-side.js
+++ core/src/elements/ons-splitter-side.js
@@ -110,7 +110,6 @@ class CollapseMode {
     this._state = CLOSED_STATE;
     this._element = element;
     this._lock = new DoorLock();
-    this._onSwipeEnd = shouldOpen => this.executeAction(shouldOpen ? 'open' : 'close');
   }
 
   isOpen() {
@@ -154,7 +153,7 @@ class CollapseMode {
     const {_distance: distance, _width: width, _element: el} = this;
     const direction = event.gesture.interimDirection;
     const shouldOpen = el._side !== direction && distance > width * el._threshold;
-    this._onSwipeEnd(shouldOpen);
+    this.executeAction(shouldOpen ? 'open' : 'close');
     this._ignoreDrag = true;
   }
 
@@ -462,19 +461,6 @@ export default class SplitterSideElement extends BaseElement {
       });
     });
   }
-
-  // This can override swiping behavior
-  get onSwipeEnd() {
-    return this._collapseMode._onSwipeEnd;
-  }
-
-  set onSwipeEnd(value) {
-    if (!(value instanceof Function)) {
-      throw new Error('SwipeEnd must be a function.');
-    }
-    this._collapseMode._onSwipeEnd = value;
-  }
-
 
   connectedCallback() {
     if (!util.match(this.parentNode, 'ons-splitter')) {
