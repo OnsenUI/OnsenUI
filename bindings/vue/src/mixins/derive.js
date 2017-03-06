718393c9a64e62dd100dbcc4c5642403da738da7
Fran Dios <frankdiox@gmail.com>
2017-03-06 15:06:32 +0900

    refactor(vue): Derive mixins.

--- bindings/vue/src/mixins/derive.js
+++ bindings/vue/src/mixins/derive.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { eventToHandler } from '../internal/util';
-import { createComputedPropertiesFor } from '../internal/optionsObjectHelper';
+import { getHandlers } from '../internal/optionsObjectHelper';
 
 const deriveEvents = {
   mounted() {
@@ -29,10 +29,8 @@ const deriveEvents = {
 const deriveHandlers = {
   mounted() {
     const dbb = 'onDeviceBackButton';
-    const nativeProps = createComputedPropertiesFor(this.$el.constructor.__proto__);
-    const propHandlers = Object.keys(nativeProps).filter(propName => /^on[A-Z]/.test(propName));
 
-    propHandlers.forEach(propName => {
+    getHandlers(this.$el.constructor.__proto__).forEach(propName => {
       const eventName = propName.slice(2).charAt(0).toLowerCase() + propName.slice(2).slice(1);
 
       if (propName === dbb) {
@@ -41,11 +39,10 @@ const deriveHandlers = {
 
         this.$el[dbb] = event => {
           const id = setTimeout(handler.bind(this.$el, event), 0);
-          const newEvent = {
+          this.$emit(eventName, {
             ...event,
             preventDefault: () => clearTimeout(id)
-          };
-          this.$emit(eventName, newEvent);
+          });
         };
 
       } else if (!this.$el[propName]) {
