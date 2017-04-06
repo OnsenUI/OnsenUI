6476bb419dca0844a2d2ae9d7c686b5e9e02fd7d
Fran Dios <frankdiox@gmail.com>
2017-04-06 17:25:40 +0900

    feat(ons-splitter): Push animation.

--- core/src/elements/ons-splitter/index.js
+++ core/src/elements/ons-splitter/index.js
@@ -19,13 +19,15 @@ import util from '../../ons/util';
 import ModifierUtil from '../../ons/internal/modifier-util';
 import AnimatorFactory from '../../ons/internal/animator-factory';
 import OverlaySplitterAnimator from './overlay-animator';
+import PushSplitterAnimator from './push-animator';
 import BaseElement from '../../ons/base-element';
 import deviceBackButtonDispatcher from '../../ons/device-back-button-dispatcher';
 import contentReady from '../../ons/content-ready';
 
 const _animatorDict = {
   default: OverlaySplitterAnimator,
-  overlay: OverlaySplitterAnimator
+  overlay: OverlaySplitterAnimator,
+  push: PushSplitterAnimator
 };
 
 /**
