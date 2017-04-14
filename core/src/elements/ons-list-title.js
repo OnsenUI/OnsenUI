c52449d326d619f12c284e0cb5c81db5cfa29ddb
Mitsunori Kubota <anatoo.jp@gmail.com>
2017-04-14 19:19:33 +0900

    feat(core): Added "ons-list-title" components.

---
+++ core/src/elements/ons-list-title.js
@@ -0,0 +1,72 @@
+/*
+Copyright 2013-2015 ASIAL CORPORATION
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+import BaseElement from '../ons/base-element';
+import autoStyle from '../ons/autostyle';
+import ModifierUtil from '../ons/internal/modifier-util';
+import util from '../ons/util';
+
+const defaultClassName = 'list-title';
+const scheme = {'': 'list-title--*'};
+
+/**
+ * @element ons-list-title
+ * @category list
+ * @description
+ *   [en]Represents a list title.[/en]
+ *   [ja]リストのタイトルを表現します。[/ja]
+ * @example
+ * <ons-list-title>List Title</ons-list-title>
+ * <ons-list>
+ *   <ons-list-item>Item</ons-list-item>
+ * </ons-list>
+ * @modifier material
+ *   [en]Display a Material Design list title.[/en]
+ *   [ja][/ja]
+ */
+
+export default class ListTitleElement extends BaseElement {
+
+  init() {
+    this._compile();
+  }
+
+  _compile() {
+    autoStyle.prepare(this);
+    this.classList.add(defaultClassName);
+    ModifierUtil.initModifier(this, scheme);
+  }
+
+  static get observedAttributes() {
+    return ['modifier', 'class'];
+  }
+
+  attributeChangedCallback(name, last, current) {
+    switch (name) {
+      case 'class':
+        if (!this.classList.contains(defaultClassName)) {
+          this.className = defaultClassName + ' ' + current;
+        }
+        break;
+      case 'modifier':
+        ModifierUtil.onModifierChanged(last, current, this, scheme);
+        break;
+    }
+  }
+}
+
+customElements.define('ons-list-title', ListTitleElement);
