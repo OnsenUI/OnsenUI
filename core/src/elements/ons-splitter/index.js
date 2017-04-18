64d9ed1dc17c074fd35d2bc144decc6e9e36877f
Fran Dios <frankdiox@gmail.com>
2017-04-18 16:44:52 +0900

    Merge pull request #1916 from OnsenUI/splitter-animations
    Splitter animations

--- core/src/elements/ons-splitter/index.js
+++ core/src/elements/ons-splitter/index.js
@@ -18,14 +18,18 @@ limitations under the License.
 import util from '../../ons/util';
 import ModifierUtil from '../../ons/internal/modifier-util';
 import AnimatorFactory from '../../ons/internal/animator-factory';
-import SplitterAnimator from './animator';
+import OverlaySplitterAnimator from './overlay-animator';
+import PushSplitterAnimator from './push-animator';
+import RevealSplitterAnimator from './reveal-animator';
 import BaseElement from '../../ons/base-element';
 import deviceBackButtonDispatcher from '../../ons/device-back-button-dispatcher';
 import contentReady from '../../ons/content-ready';
 
 const _animatorDict = {
-  default: SplitterAnimator,
-  overlay: SplitterAnimator
+  default: OverlaySplitterAnimator,
+  overlay: OverlaySplitterAnimator,
+  push: PushSplitterAnimator,
+  reveal: RevealSplitterAnimator
 };
 
 /**
