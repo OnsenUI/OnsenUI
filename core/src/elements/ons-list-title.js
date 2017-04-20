b2177bd7b1fd409c56cafc1e5f7cf10a92300827
Mitsunori Kubota <anatoo.jp@gmail.com>
2017-04-20 12:26:28 +0900

    fix(ons-list-title): Fixed import errors.

--- core/src/elements/ons-list-title.js
+++ core/src/elements/ons-list-title.js
@@ -15,7 +15,7 @@ limitations under the License.
 
 */
 
-import BaseElement from '../ons/base-element';
+import BaseElement from './base/base-element';
 import autoStyle from '../ons/autostyle';
 import ModifierUtil from '../ons/internal/modifier-util';
 import util from '../ons/util';
