a680d77509529f23f4e917f4b3484fea1af87da9
Mister Júnio <ruca.33.maia@gmail.com>
2017-04-28 12:46:24 +0900

    test(ons-card): Removed forgotten log.

--- bindings/angular2/examples/card.spec.js
+++ bindings/angular2/examples/card.spec.js
@@ -4,7 +4,6 @@ describe('card.html', () => {
   });
 
   it('should have ons-card elements', () => {
-    console.log('\n\n\n\nCARD TEST\n\n\n\n');
     expect($('ons-card').isPresent()).toBeTruthy();
     expect($('div.title').isPresent()).toBeTruthy();
     expect($('div.title').getText()).toEqual('Awesome card title');
