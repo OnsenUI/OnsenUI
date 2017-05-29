c4cea4b86e70122c5603b9c42f88cba2b0e0d539
Fran Dios <frankdiox@gmail.com>
2017-05-30 00:48:20 +0900

    chore(unit): Use headless Chrome for unit testing.

--- core/test/unit/karma.conf.js
+++ core/test/unit/karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
       // Desktop - Chrome
       ////////////////////////////////////////
       local_chrome: { // alias for `Chrome` (defined by `karma-chrome-launcher`)
-        base: 'Chrome',
+        base: 'ChromeHeadless',
       },
 
       ////////////////////////////////////////
