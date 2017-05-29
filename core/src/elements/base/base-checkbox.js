97706299f24dc75f080c2292c1c20ba6290fadf1
Fran Dios <frankdiox@gmail.com>
2017-05-30 04:03:03 +0900

    refactor(ons-input): Add base checkbox element.

---
+++ core/src/elements/base/base-checkbox.js
@@ -0,0 +1,66 @@
+/*
+Copyright 2013-2015 ASIAL CORPORATION
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import BaseInputElement from './base-input';
+import contentReady from '../../ons/content-ready';
+
+export default class BaseCheckboxElement extends BaseInputElement {
+
+  get _defaultElementClass() {
+    throw new Error('_defaultElementClass getter must be implemented.');
+  }
+
+  constructor() {
+    super();
+
+    contentReady(this, () => {
+      this.attributeChangedCallback('checked', null, this.getAttribute('checked'));
+    });
+  }
+
+  _addClassesAndUpdate() {
+    this.classList.add(this._defaultElementClass);
+    this._input.classList.add(`${this._defaultElementClass}__input`);
+    this._helper.classList.add(`${this._defaultElementClass}__checkmark`);
+    this._updateBoundAttributes();
+  }
+
+  get checked() {
+    return this._input.checked;
+  }
+
+  set checked(val) {
+    contentReady(this, () => {
+      this._input.checked = val;
+    });
+  }
+
+  static get observedAttributes() {
+    return [...super.observedAttributes, 'class', 'checked'];
+  }
+
+  attributeChangedCallback(name, last, current) {
+    switch (name) {
+      case 'checked':
+        this.checked = current !== null;
+        break;
+      case 'class':
+        if (!this.classList.contains(this._defaultElementClass)) {
+          this.className = this._defaultElementClass + ' ' + current;
+        }
+        break;
+      default:
+        super.attributeChangedCallback(name, last, current);
+    }
+  }
+}
