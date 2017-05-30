cd2491882d8767712aa2ce58935abd4c339cfff4
Naoki Matagawa <naoki.m@asial.co.jp>
2017-05-30 11:36:33 +0900

    chore(unit): Add `local_chrome_headless`.

--- core/test/unit/karma.conf.js
+++ core/test/unit/karma.conf.js
@@ -82,6 +82,9 @@ module.exports = function(config) {
       // Desktop - Chrome
       ////////////////////////////////////////
       local_chrome: { // alias for `Chrome` (defined by `karma-chrome-launcher`)
+        base: 'Chrome',
+      },
+      local_chrome_headless: { // alias for `ChromeHeadless` (defined by `karma-chrome-launcher`)
         base: 'ChromeHeadless',
       },
 
