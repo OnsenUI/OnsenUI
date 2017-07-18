cc75d80d04d0558cc3d2e0ede7d1d75eb3900f86
Mister Júnio <ruca.33.maia@gmail.com>
2017-07-18 15:11:22 +0900

    feat(core): Added basics for segment component.

---
+++ core/src/elements/ons-segment.js
@@ -0,0 +1,112 @@
+/*
+Copyright 2013-2015 ASIAL CORPORATION
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+import util from '../ons/util';
+import autoStyle from '../ons/autostyle';
+import ModifierUtil from '../ons/internal/modifier-util';
+import BaseElement from './base/base-element';
+
+const defaultClassName = 'segment';
+const scheme = {
+  '': 'segment--*',
+  '.segment__item': 'segment--*__item',
+  '.segment__input': 'segment--*__input',
+  '.segment__button': 'segment--*__button'
+};
+
+/**
+ * @element ons-segment
+ * @category control
+ * @modifier material
+ *   [en]Material Design segment[/en]
+ *   [ja][/ja]
+ * @description
+ *   [en]
+ *     Segment component. Use this component to have a button bar with automatic styles that switch on click of another button.
+ *
+ *     Will automatically display as a Material Design segment on Android.
+ *   [/en]
+ *   [ja][/ja]
+ * @codepen hLayx
+ * @tutorial vanilla/Reference/segment
+ * @guide using-modifier [en]More details about the `modifier` attribute[/en][ja]modifier属性の使い方[/ja]
+ * @guide cross-platform-styling [en]Information about cross platform styling[/en][ja]Information about cross platform styling[/ja]
+ * @example
+ * <ons-segment>
+ *   <ons-button>Label 1</ons-button>
+ *   <ons-button>Label 2</ons-button>
+ *   <ons-button>Label 3</ons-button>
+ * </ons-segment>
+ */
+
+export default class SegmentElement extends BaseElement {
+
+  /**
+   * @attribute modifier
+   * @type {String}
+   * @description
+   *  [en]The appearance of the button.[/en]
+   *  [ja]ボタンの表現を指定します。[/ja]
+   */
+
+  /**
+   * @attribute ripple
+   * @description
+   *  [en]If this attribute is defined, the button will have a ripple effect.[/en]
+   *  [ja][/ja]
+   */
+
+  /**
+   * @attribute disabled
+   * @description
+   *   [en]Specify if button should be disabled.[/en]
+   *   [ja]ボタンを無効化する場合は指定します。[/ja]
+   */
+
+  constructor() {
+    super();
+
+    this._compile();
+  }
+
+  _compile() {
+    autoStyle.prepare(this);
+
+    this.classList.add(defaultClassName);
+
+    ModifierUtil.initModifier(this, scheme);
+  }
+
+  static get observedAttributes() {
+    return ['class', 'modifier'];
+  }
+
+  attributeChangedCallback(name, last, current) {
+    switch (name) {
+      case 'class':
+        if (!this.classList.contains(defaultClassName)) {
+          this.className = defaultClassName + ' ' + current;
+        }
+        break;
+      case 'modifier':
+        ModifierUtil.onModifierChanged(last, current, this, scheme);
+        break;
+    }
+  }
+}
+
+customElements.define('ons-segment', SegmentElement);
---
+++ core/src/elements/ons-segment.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+describe('ons-segment', () => {
+  it('provides \'OnsSegmentElement\' global variable', () => {
+    expect(window.ons.SegmentElement).to.be.ok;
+  });
+
+  describe('class attribute', () => {
+    it('should contains "segment" class name automatically', () => {
+      const element = new ons.SegmentElement();
+      element.setAttribute('class', 'foobar');
+      expect(element.classList.contains('segment')).to.be.true;
+      expect(element.classList.contains('foobar')).to.be.true;
+    });
+  });
+
+  it('provides modifier attribute', () => {
+    var element = new ons.SegmentElement();
+    element.setAttribute('modifier', 'hoge');
+    expect(element.classList.contains('segment--hoge')).to.be.true;
+
+    element.setAttribute('modifier', 'foo bar');
+    expect(element.classList.contains('segment--foo')).to.be.true;
+    expect(element.classList.contains('segment--bar')).to.be.true;
+    expect(element.classList.contains('segment--hoge')).not.to.be.true;
+
+    element.classList.add('segment--piyo');
+    element.setAttribute('modifier', 'fuga');
+    expect(element.classList.contains('segment--piyo')).to.be.true;
+    expect(element.classList.contains('segment--fuga')).to.be.true;
+  });
+
+  describe('#_compile()', () => {
+    it('does not compile twice', () => {
+      const div1 = document.createElement('div');
+      const div2 = document.createElement('div');
+      div1.innerHTML = `
+        <ons-segment>
+          <ons-button>Label 1</ons-button>
+          <ons-button>Label 2</ons-button>
+          <ons-button>Label 3</ons-button>
+        </ons-segment>
+      `;
+      div2.innerHTML = div1.innerHTML;
+      expect(div1.isEqualNode(div2)).to.be.true;
+    });
+  });
+});
+
