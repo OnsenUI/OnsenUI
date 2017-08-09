e8aaf508022835e40c58fa12c7a82d8bdd1e6715
Fran Dios <frankdiox@gmail.com>
2017-08-09 16:37:27 +0900

    docs(ons-fab): Add missing 'mini' modifier.

--- core/src/elements/ons-fab.js
+++ core/src/elements/ons-fab.js
@@ -35,6 +35,9 @@ const scheme = {
  *   [/en]
  *   [ja][/ja]
  * @tutorial vanilla/Reference/fab
+ * @modifier mini
+ *   [en]Makes the `ons-fab` smaller.[/en]
+ *   [ja][/ja]
  * @guide cross-platform-styling [en]Information about cross platform styling[/en][ja]Information about cross platform styling[/ja]
  * @seealso ons-speed-dial
  *   [en]The `<ons-speed-dial>` component is a Floating action button that displays a menu when tapped.[/en]
