d553643e93870bd2adec5a841b9796633b074f88
Fran Dios <frankdiox@gmail.com>
2017-09-07 12:17:10 +0900

    fix(ons-card, ons-list): Add missing imports.

--- core/src/elements/ons-list.js
+++ core/src/elements/ons-list.js
@@ -15,6 +15,7 @@ limitations under the License.
 
 */
 
+import util from '../ons/util';
 import autoStyle from '../ons/autostyle';
 import ModifierUtil from '../ons/internal/modifier-util';
 import BaseElement from './base/base-element';
