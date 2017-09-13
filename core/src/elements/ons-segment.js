eedf5679ee32631c12dd5a2e790d460bedbb89f4
Fran Dios <frankdiox@gmail.com>
2017-09-13 16:41:26 +0900

    fix(ons-segment): Update modifier classes.

--- core/src/elements/ons-segment.js
+++ core/src/elements/ons-segment.js
@@ -164,9 +164,7 @@ export default class SegmentElement extends BaseElement {
   attributeChangedCallback(name, last, current) {
     switch (name) {
       case 'class':
-        if (!this.classList.contains(defaultClassName)) {
-          this.className = defaultClassName + ' ' + current;
-        }
+        util.restoreClass(this, defaultClassName, scheme);
         break;
       case 'modifier':
         ModifierUtil.onModifierChanged(last, current, this, scheme);
