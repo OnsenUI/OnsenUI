2c8ff112b8cf219672377269e2b2e70e059cd204
Fran Dios <frankdiox@gmail.com>
2017-09-15 18:36:55 +0900

    feat(angular1): Add ons-postchange handler and docs.

--- bindings/angular1/directives/segment.js
+++ bindings/angular1/directives/segment.js
@@ -1,3 +1,25 @@
+/**
+ * @element ons-segment
+ */
+
+/**
+ * @attribute var
+ * @initonly
+ * @type {String}
+ * @description
+ *   [en]Variable name to refer this segment.[/en]
+ *   [ja]このタブバーを参照するための名前を指定します。[/ja]
+ */
+
+/**
+ * @attribute ons-postchange
+ * @initonly
+ * @type {Expression}
+ * @description
+ *  [en]Allows you to specify custom behavior when the "postchange" event is fired.[/en]
+ *  [ja]"postchange"イベントが発火された時の挙動を独自に指定できます。[/ja]
+ */
+
 (function() {
   'use strict';
 
@@ -5,8 +27,9 @@
     return {
       restrict: 'E',
       link: function(scope, element, attrs) {
-        GenericView.register(scope, element, attrs, {viewKey: 'ons-segment'});
+        var view = GenericView.register(scope, element, attrs, {viewKey: 'ons-segment'});
         $onsen.fireComponentEvent(element[0], 'init');
+        $onsen.registerEventHandlers(view, 'postchange');
       }
     };
   });
