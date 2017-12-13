88f8e42f889974d38c94ee4d58bd897e13fa9b08
Fran Dios <frankdiox@gmail.com>
2017-12-13 20:39:08 +0900

    docs(angular1): Splitter components 'var' attr.

--- bindings/angular1/directives/splitterSide.js
+++ bindings/angular1/directives/splitterSide.js
@@ -3,6 +3,15 @@
  */
 
 /**
+ * @attribute var
+ * @initonly
+ * @type {String}
+ * @description
+ *   [en]Variable name to refer this splitter side.[/en]
+ *   [ja]このスプリッターコンポーネントを参照するための名前を指定します。[/ja]
+ */
+
+/**
  * @attribute ons-destroy
  * @initonly
  * @type {Expression}
