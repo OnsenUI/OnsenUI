6742c625dd9e0784bbda78e3cc2df720813fedf5
Fran Dios <frankdiox@gmail.com>
2018-01-31 10:42:35 +0900

    refactor(ons.notification): Use 'const'.

--- core/src/ons/notification.js
+++ core/src/ons/notification.js
@@ -224,7 +224,7 @@ notification._createAlertDialog = (...params) => new Promise(resolve => {
     el.dialog.show()
       .then(() => {
         if (el.input && options.isPrompt && options.autofocus) {
-          var strLength = el.input.value.length;
+          const strLength = el.input.value.length;
           el.input.focus();
           el.input.setSelectionRange(strLength, strLength);
         }
