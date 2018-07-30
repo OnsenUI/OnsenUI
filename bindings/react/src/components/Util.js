1ebda49252e410623f986a0e5a27487a305ca121
Eric Corson <emccorson@gmail.com>
2018-07-30 19:42:24 +0900

    refactor(react): Refactor utils.

--- bindings/react/src/components/Util.js
+++ bindings/react/src/components/Util.js
@@ -1,5 +1,3 @@
-const pixelSize = item => typeof item === 'number' ? `${item}px` : item;
-
 const normalize = key => {
   if (/^is[A-Z]/.test(key)) {
     key = key.slice(2);
@@ -7,59 +5,56 @@ const normalize = key => {
   return key.replace(/([a-zA-Z])([A-Z])/g, '$1-$2').toLowerCase();
 };
 
-const convert = (dict, name, newName = name) => {
-  if (dict.hasOwnProperty(name)) {
-    switch (typeof dict[name]) {
-      case 'boolean':
-        if (dict[name]) {
-          dict[newName] = '';
-        } else {
-          delete dict[newName];
-        }
-        break;
-      case 'string':
-      case 'number':
-        dict[newName] = dict[name];
-        break;
-      default:
-        dict[name] = null;
-    }
-
-    if (newName !== name) {
-      dict[name] = null;
-    }
-  }
-
-  return dict;
-};
-
 export default {
   eventToHandler(string) {
     return 'on' + string.charAt(0).toUpperCase() + string.slice(1);
   },
 
-  getAttrs(el, props = el.props, dict = {}) {
-    const attrs = { ...props };
+  /**
+   * Turns an object of React props into an object of vanilla JS properties for a given component.
+   *
+   * @param {Object} el
+   *   The component whose props should be converted
+   * @param {Object} props
+   *   The props that should be converted (default: all props)
+   * @param {Object} nameMap
+   *   Map of 'react prop name' -> 'vanilla JS property name'. Overrides default renaming scheme.
+   */
+  getAttrs(el, props = el.props, nameMap = {}) {
+    const jsProperties = {};
     const validProps = el.constructor.propTypes || {};
+    const ignoredProps = ['isOpen'];
 
-    if (attrs.hasOwnProperty('animationOptions') && typeof attrs.animationOptions !== 'string') {
-      attrs.animationOptions = JSON.stringify(attrs.animationOptions);
-    }
+    Object.keys(props).forEach(reactName => {
+      const reactValue = props[reactName];
 
-    Object.keys(attrs)
-      .forEach(key => {
-        if (validProps.hasOwnProperty(key) && ['onClick'].indexOf(key) === -1) {
-          if (['isOpen'].indexOf(key) !== -1) {
-            attrs[key] = null;
+      // onClick and anything that isn't a valid React prop get added immediately
+      if (!validProps.hasOwnProperty(reactName) || reactName === 'onClick') {
+        jsProperties[reactName] = reactValue;
+
+      // don't add any props we specifically want to ignore
+      } else if (ignoredProps.indexOf(reactName) === -1) {
+        const jsName = nameMap[reactName] || normalize(reactName);
+        const type = typeof reactValue;
+
+        if (type === 'boolean' && reactValue) {
+          jsProperties[jsName] = '';
+        } else if (type === 'string') {
+          if (reactName === 'animationOptions') {
+            jsProperties[jsName] = JSON.stringify(reactValue);
           } else {
-            if (/(height|width)/i.test(key)) {
-              attrs[key] = pixelSize(attrs[key]);
-            }
-            convert(attrs, key, dict[key] || normalize(key));
+            jsProperties[jsName] = reactValue;
+          }
+        } else if (type === 'number') {
+          if (/(height|width)/i.test(reactName)) {
+            jsProperties[jsName] = reactValue + 'px';
+          } else {
+            jsProperties[jsName] = reactValue;
           }
         }
-      });
+      }
+    });
 
-    return attrs;
+    return jsProperties;
   }
 };
