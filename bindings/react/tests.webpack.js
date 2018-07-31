52475556b332b337d7b5b4dca011994fe8b4299e
Eric Corson <emccorson@gmail.com>
2018-07-31 11:49:18 +0900

    test(react): Add Enzyme for writing tests.

--- bindings/react/tests.webpack.js
+++ bindings/react/tests.webpack.js
@@ -1,2 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 var context = require.context('./test', true, /-test\.js$/);
 context.keys().forEach(context);
