5dad77db02ab2a224bc7cfafe9b83a3aefdb64ae
Edward Griffiths <edward@afrigis.co.za>
2018-09-25 16:27:43 +0200

    Added Extra sizes for iphone XR

--- core/src/ons/platform.js
+++ core/src/ons/platform.js
@@ -109,9 +109,12 @@ class Platform {
   isIPhoneX() {
     // iPhone 8 and iPhone X have a same user agent. We cannot avoid using window.screen.
     // This works well both in iOS Safari and (UI|WK)WebView of iPhone X.
+    //Added extra sizes for IPhone XR
     return this.isIPhone() &&
-      (window.screen.width === 375 && window.screen.height === 812
-    || window.screen.width === 812 && window.screen.height === 375);
+      (window.screen.width === 375 && window.screen.height === 812 ||
+       window.screen.width === 414 && window.screen.height === 896 ||
+       window.screen.width === 896 && window.screen.height === 414 ||
+       window.screen.width === 812 && window.screen.height === 375);
   }
 
   /**
