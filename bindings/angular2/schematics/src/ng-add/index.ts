3ee48220325a6c1a29f84929bf85679bd1f57e99
puku0x <puku0x@gmail.com>
2019-08-31 20:33:46 +0900

    chore(ngx): fix comments

--- bindings/angular2/schematics/src/ng-add/index.ts
+++ bindings/angular2/schematics/src/ng-add/index.ts
@@ -6,10 +6,7 @@ import { Schema } from './schema';
 
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
- * automatically executed if developers run `ng add @angular/material`.
- *
- * Since the ngx-onsenui schematics depend on the schematic utility functions from the CDK,
- * we need to install the CDK before loading the schematic files that import from the CDK.
+ * automatically executed if developers run `ng add ngx-onsenui`.
  */
 export default function(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
