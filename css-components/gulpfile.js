fdba424623b912b276597d3aab6a7a4f8421d05e
Eric Corson <emccorson@gmail.com>
2019-12-03 04:35:03 +0000

    chore(css-components): Replace deprecated postcss-cssnext
    postcss-apply was also added since postcss-preset-env does not support
    the @apply rule:
    https://github.com/csstools/postcss-preset-env/issues/50

--- css-components/gulpfile.js
+++ css-components/gulpfile.js
@@ -7,7 +7,7 @@ const $ = require('gulp-load-plugins')();
 const eco = require('eco');
 const fs = require('fs');
 const ancss = require('@onsenui/ancss');
-const cssnextPlugin = require('postcss-cssnext');
+const cssnextPlugin = require('postcss-preset-env');
 const reporter = require('postcss-reporter');
 const historyApiFallback = require('connect-history-api-fallback');
 const {rollup} = require('rollup');
@@ -79,6 +79,7 @@ function cssnext() {
       extensions: ['.svg'],
       root: __dirname + '/src/components/'
     }),
+    require('postcss-apply'),
     cssnextPlugin({
       browsers: babelrc.presets[0][1].targets.browsers,
     }),
