6b5cbad130b6ae97b22971d7432a998ecdd0f6e6
Eric Corson <emccorson@gmail.com>
2021-02-08 14:54:16 +0900

    fix(vue): Use app instance instead of global Vue in UMD build for Vue 3

--- bindings/vue/vue-onsenui/src/index.umd.js
+++ bindings/vue/vue-onsenui/src/index.umd.js
@@ -4,21 +4,18 @@ import * as components from './components';
 
 const $ons = setup(ons);
 
-$ons.install = Vue => {
+$ons.install = app => {
+
   /**
    * Register components of vue-onsenui.
    */
   Object.keys(components)
-    .forEach(key => Vue.component(components[key].name, components[key]));
+    .forEach(key => app.component(components[key].name, components[key]));
 
   /**
    * Expose ons object.
    */
-  Vue.prototype.$ons = $ons;
+  app.config.globalProperties.$ons = $ons;
 };
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use({ install: $ons.install });
-}
 
 export default $ons;
