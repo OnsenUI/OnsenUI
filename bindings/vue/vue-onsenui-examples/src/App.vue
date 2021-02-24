5a495b2946e1da3674e4e2d8e1e6598b1bb3602a
Eric Corson <emccorson@gmail.com>
2021-02-24 15:23:47 +0000

    fix(vue): Split vue examples entry file into single file components
    The examples app now loads the list of examples properly but there are
    still lots of warnings and lots of examples that are not working.

---
+++ bindings/vue/vue-onsenui-examples/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar>
+
+      <div class="left">
+        <v-ons-toolbar-button
+          @click.prevent="backToList"
+          v-show="currentView !== mainList"
+        >
+          Main list
+        </v-ons-toolbar-button>
+      </div>
+
+      <div class="center">{{ title }}</div>
+    </v-ons-toolbar>
+
+    <div class="content">
+      <keep-alive>
+        <component
+          :is="currentView"
+          :examples="examples"
+          :changeExample="changeExample"
+        ></component>
+      </keep-alive>
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+
+import * as examples from './components';
+import MainList from './MainList.vue';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      MainList,
+      examples,
+      title: 'Main List',
+      currentView: MainList
+    }
+  },
+
+  methods: {
+    changeExample(key) {
+      this.title = key;
+      this.currentView = this.examples[key];
+    },
+    backToList() {
+      this.title = 'Main List';
+      this.currentView = MainList;
+    }
+  }
+}
+</script>
