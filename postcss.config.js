1994d33302fb7c35d91c9897dc790b7678415acc
Eric Corson <emccorson@gmail.com>
2021-08-10 08:12:45 +0100

    chore(*): Move css-components source to the root package

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,23 @@
 module.exports = {
   plugins: [
     require('postcss-import')({
-      filter: id => !/\.\/(font_awesome|ionicons|material-design-iconic-font)\//.test(id)
+      filter: (id) => !/\.\/(font_awesome|ionicons|material-design-iconic-font)\//.test(id)
     }),
-    require('autoprefixer')({ remove: false })
-  ],
+    require('postcss-base64')({
+      extensions: ['.svg'],
+      root: 'css/css-components-src/components/'
+    }),
+    require('postcss-mixins'),
+    require('postcss-custom-media')({
+      preserve: false
+    }),
+    require('postcss-nested'),
+    require('postcss-color-mod-function'),
+    require('postcss-preset-env'),
+    require('postcss-reporter')({
+      clearAllMessages: true,
+      clearReportedMessages: true,
+      throwError: false
+    })
+  ]
 }
