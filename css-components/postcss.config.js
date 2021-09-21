24aa916553ba637a45ae3d7a653dc97b67ae6f23
Eric Corson <emccorson@gmail.com>
2021-09-21 14:02:35 +0100

    chore(css-components): Remove postcss-custom-media

--- css-components/postcss.config.js
+++ css-components/postcss.config.js
@@ -3,9 +3,6 @@ const browsers = require('../package.json')['browserslist'];
 module.exports = {
   plugins: [
     require('postcss-import')({ from: null }),
-    require('postcss-custom-media')({
-      preserve: false
-    }),
     require('postcss-nested'),
     require('postcss-preset-env')({
       browsers: browsers
