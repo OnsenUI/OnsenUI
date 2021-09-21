dae213d7fc0b1fb77d1aefbddbe944a0c9502df9
Eric Corson <emccorson@gmail.com>
2021-09-21 14:48:31 +0100

    chore(css-components): Remove postcss-nested

--- css-components/postcss.config.js
+++ css-components/postcss.config.js
@@ -3,7 +3,6 @@ const browsers = require('../package.json')['browserslist'];
 module.exports = {
   plugins: [
     require('postcss-import')({ from: null }),
-    require('postcss-nested'),
     require('postcss-preset-env')({
       browsers: browsers
     }),
