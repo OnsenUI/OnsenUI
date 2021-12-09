7aadc32ce738fd8e75a390b843ecb84a6552a2e0
Eric Corson <emccorson@gmail.com>
2021-12-09 15:39:10 +0000

    chore(*): Fix unit testing
    Bump karma dependencies and update karma config file.

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,6 @@
 // Karma configuration
 // Generated on Thu Apr 09 2015 15:16:41 GMT+0900 (JST)
 
-var babelrc = require('./package.json').babel;
-babelrc.babelrc = babelrc.presets[0][1].modules = false;
-babelrc.plugins = ['external-helpers'];
-
 module.exports = function(config) {
   config.set({
 
@@ -38,14 +34,15 @@ module.exports = function(config) {
 
 		rollupPreprocessor: {
 			plugins: [
-        require('rollup-plugin-string')({ include: '**/*.svg'}),
 				require('rollup-plugin-node-resolve')(),
         require('rollup-plugin-commonjs')({ include: 'node_modules/**' }),
-				require('rollup-plugin-babel')(babelrc)
+				require('@rollup/plugin-babel').babel({ babelHelpers: 'bundled' })
 			],
-			format: 'iife',         // Helps prevent naming collisions.
-			name: 'onsTest', // Required for 'iife' format.
-			sourcemap: 'inline'     // Sensible for testing.
+      output: {
+			  format: 'iife',         // Helps prevent naming collisions.
+			  name: 'onsTest', // Required for 'iife' format.
+			  sourcemap: 'inline'     // Sensible for testing.
+      }
 		},
 
     // list of files to exclude
