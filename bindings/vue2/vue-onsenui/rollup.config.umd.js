67d24157374c44b943a071fb550b83b549d39934
Eric Corson <emccorson@gmail.com>
2022-02-15 21:53:42 +0000

    chore(vue2): Bump rollup plugin node resolve

--- bindings/vue2/vue-onsenui/rollup.config.umd.js
+++ bindings/vue2/vue-onsenui/rollup.config.umd.js
@@ -4,7 +4,7 @@ import dateformat from 'dateformat';
 // Rollup plugins
 import { babel } from '@rollup/plugin-babel';
 import eslint from '@rollup/plugin-eslint';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import progress from 'rollup-plugin-progress';
 import vue from 'rollup-plugin-vue';
