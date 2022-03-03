78d75d760319ef9f34d8120c72d4470c463aea5d
Eric Corson <emccorson@gmail.com>
2022-03-03 15:49:21 +0000

    fix(react): Add onsCustomElement HOC to replace SimpleWrapper

---
+++ react-onsenui/src/components/onsCustomElement.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const kebabize = camelString =>
+  camelString.replace(/([a-zA-Z])([A-Z])/g, '$1-$2').toLowerCase();
+
+function useCustomElementListener(ref, prop, handler) {
+  const event = prop.slice(2).toLowerCase();
+  useEffect(() => {
+    ref.current.addEventListener(event, handler);
+
+    return function cleanup() {
+      ref.current.removeEventListener(event, handler);
+    };
+  }, [ref, handler]);
+}
+
+function useCustomElement(props, propTypes, notAttributes = []) {
+  const ref = useRef();
+
+  const properties = {};
+
+  for (const [prop, value] of Object.entries(props)) {
+    const jsName = kebabize(prop);
+
+    if (notAttributes.includes(prop)) {
+      useEffect(() => {
+        ref.current[prop] = value;
+      });
+    } else if (/^on[A-Z]/.test(prop)) {
+      useCustomElementListener(ref, prop, value);
+    } else if (propTypes[prop] === PropTypes.bool) {
+      properties[jsName] = value ? '' : null;
+    } else {
+      properties[jsName] = value;
+    }
+  }
+
+  return {ref, properties};
+}
+
+export default function onsCustomElement(WrappedComponent, propTypes, notAttributes) {
+  return function (props) {
+
+    const {ref, properties} = useCustomElement(props, propTypes, notAttributes);
+
+    return (
+      <WrappedComponent
+        ref={ref}
+        {...properties}
+      >
+        {props.children}
+      </WrappedComponent>
+    );
+  };
+}
