ad734c8c2f8c587535098a5da02f0185b9ad1bad
Eric Corson <emccorson@gmail.com>
2022-05-19 12:24:36 +0900

    fix(vue): Make VOnsListItem use core expanded property

--- vue-onsenui/src/components/VOnsListItem.vue
+++ vue-onsenui/src/components/VOnsListItem.vue
@@ -1,33 +1,19 @@
 <template>
-  <ons-list-item v-on="unrecognizedListeners" :modifier="normalizedModifier" v-on:expansion="onExpansion">
+  <ons-list-item v-on="unrecognizedListeners" :modifier="normalizedModifier" v-on:expand="onExpand">
     <slot></slot>
   </ons-list-item>
 </template>
 
 <script>
-  /* This file was generated automatically by 'generate-components' task in bindings/vue/gulpfile.babel.js */
   import 'onsenui/esm/elements/ons-list-item';
   import { deriveEvents, modifier } from '../mixins';
 
   export default {
     name: 'v-ons-list-item',
     mixins: [deriveEvents, modifier],
-    props: {
-      expanded: {
-        type: Boolean
-      }
-    },
     methods: {
-      onExpansion() {
-        if (this.expanded !== this.$el.expanded) {
-          this.$emit('update:expanded', this.$el.expanded);
-        }
-      }
-    },
-    watch: {
-      expanded() {
-        const action = this.expanded ? 'show' : 'hide';
-        this.$el[action + 'Expansion']();
+      onExpand() {
+        this.$emit('update:expanded', this.$el.expanded);
       }
     }
   };
