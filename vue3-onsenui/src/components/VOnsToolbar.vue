4bf846508b6dbba80b02fd3babe57d1932832a63
Eric Corson <emccorson@gmail.com>
2022-06-20 11:17:01 +0900

    fix(vue3): Remove hidable mixin from VOnsToolbar
    It already has a core visible property so the mixin is not needed.

--- vue3-onsenui/src/components/VOnsToolbar.vue
+++ vue3-onsenui/src/components/VOnsToolbar.vue
@@ -7,12 +7,12 @@
 <script>
   /* This file was generated automatically by 'generate-components' task in bindings/vue/gulpfile.babel.js */
   import 'onsenui/esm/elements/ons-toolbar.js';
-  import { deriveEvents, hidable, modifier } from '../mixins/index.js';
+  import { deriveEvents, modifier } from '../mixins/index.js';
 
   const name = 'v-ons-toolbar';
 
   export default {
     name,
-    mixins: [deriveEvents(name), hidable, modifier]
+    mixins: [deriveEvents(name), modifier]
   };
 </script>
