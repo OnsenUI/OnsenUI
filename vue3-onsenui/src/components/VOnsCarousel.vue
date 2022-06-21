ba95b2154f66f441b53dd201ba5a40cc96746a59
Eric Corson <emccorson@gmail.com>
2022-06-21 15:21:38 +0900

    fix(vue3): Remove unused options prop from VOnsCarousel

--- vue3-onsenui/src/components/VOnsCarousel.vue
+++ vue3-onsenui/src/components/VOnsCarousel.vue
@@ -11,13 +11,13 @@
 
 <script>
   import 'onsenui/esm/elements/ons-carousel.js';
-  import { hasOptions, deriveEvents } from '../mixins/index.js';
+  import { deriveEvents } from '../mixins/index.js';
 
   const name = 'v-ons-carousel';
 
   export default {
     name,
-    mixins: [hasOptions, deriveEvents(name)],
+    mixins: [deriveEvents(name)],
     emits: ['update:activeIndex'],
   };
 </script>
