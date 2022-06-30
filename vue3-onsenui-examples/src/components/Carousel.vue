fbc9225e8bd47471b3392b5fa5891bc75f236a8e
Eric Corson <public@giraffedev.com>
2022-07-01 08:58:26 +0900

    examples(vue3): Replace index with activeIndex

--- vue3-onsenui-examples/src/components/Carousel.vue
+++ vue3-onsenui-examples/src/components/Carousel.vue
@@ -10,7 +10,7 @@
       </div>
     </v-ons-toolbar>
 
-    <v-ons-carousel fullscreen v-model:index="carouselIndex" swipeable auto-scroll overscrollable @postchange="log('postchange')" @refresh="log('refresh')" @overscroll="log('overscroll')">
+    <v-ons-carousel fullscreen v-model:active-index="carouselIndex" swipeable auto-scroll overscrollable @postchange="log('postchange')" @refresh="log('refresh')" @overscroll="log('overscroll')">
       <v-ons-carousel-item v-for="(value, key) in items" :key="key" :style="{backgroundColor: value}">
         <div style="text-align: center; font-size: 30px; margin-top: 20px; color: #fff;">{{key}}</div>
       </v-ons-carousel-item>
